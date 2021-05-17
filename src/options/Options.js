6d7b32b1435cc85227f44444be5af07e3c074374
minhvu-eastagile <minh.vu@eastagile.com>
2021-05-17 16:13:46 +0700

    [f] 504 - Gallery UI

--- src/options/Options.js
+++ src/options/Options.js
@@ -1,27 +1,79 @@
-import React, { Component } from 'react'
-import '@polymer/paper-button/paper-button.js'
+import React, { useMemo, useState } from 'react'
+import { Link } from 'react-router-dom'
+import find from 'lodash/find'
+
+import KoiIcon from 'img/koi-logo.svg'
+import KoiUnit from 'img/koi-logo-no-bg.svg'
+import SettingsIcon from 'img/settings-icon.svg'
+import ShareIcon from 'img/share-icon.svg'
 
 import './Options.css'
 
-class Options extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Koi
-          </a>
-          <h1>Your gallery</h1>
-          <paper-button toggles raised class="green">toggles</paper-button>
-        </header>
+const Header = ({ totalKoi }) => {
+  return (
+    <header className="app-header">
+      <div className="header-left">
+        <KoiIcon className="logo" />
       </div>
-    )
-  }
+      <div className="header-right">
+        <div className="total-koi">
+          <div>
+            {totalKoi}
+          </div>
+          <KoiUnit className="koi-unit" />
+        </div>
+        <button className="setting-button"><SettingsIcon /></button>
+      </div>
+    </header>
+  )
 }
 
-export default Options
+const BigCard = ({ txId, name, imgSrc, earnedKoi, isRegistered, path, setChoosen }) => {
+  return (
+    <div className="big-nft-card" onClick={() => setChoosen('')}>
+      <img src={imgSrc} className="nft-img" />
+      <div className="nft-name">{name}</div>
+      {isRegistered ? <div className="nft-earned-koi">{earnedKoi} KOI</div> : <button className="register-button"><KoiIcon className="icon" /> Register &amp; Earn</button>}
+      {isRegistered && <Link to={path} className="nft-path">View on koi.rocks</Link>}
+    </div>
+  )
+}
+
+const Card = ({ txId, name, imgSrc, earnedKoi, isRegistered, path, choosen, setChoosen }) => {
+  return (
+    choosen !== txId
+      ? <div className="nft-card" onClick={() => setChoosen(txId)}>
+        <img src={imgSrc} className="nft-img" />
+        <div className="nft-name">{name}</div>
+        {isRegistered ? <div className="nft-earned-koi">{earnedKoi} KOI</div> : <button className="register-button"><KoiIcon className="icon" /> Register &amp; Earn</button>}
+        {isRegistered && <Link to={path} className="nft-path"><ShareIcon /></Link>}
+      </div >
+      : null
+  )
+}
+
+const Content = ({ cardInfos }) => {
+  const [choosen, setChoosen] = useState('')
+  const choosenCard = useMemo(() => find(cardInfos, { txId: choosen }), [choosen])
+
+  return (
+    <div className="app-content">
+      <div className="title">My NFT Gallery</div>
+      <div className="cards">
+        {choosen && <BigCard {...choosenCard} setChoosen={setChoosen} />}
+        <div className="small-cards">
+          {cardInfos.map(cardInfo => <Card choosen={choosen} setChoosen={setChoosen} {...cardInfo} />)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ({ cardInfos = [], totalKoi = 0 }) => {
+  return (
+    <div className="app">
+      <Header totalKoi={totalKoi} />
+      <Content cardInfos={cardInfos} />
+    </div >
+  )
+}
