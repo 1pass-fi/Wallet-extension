1e81e868ea9bcca62a913e1af1ad80b4a160c0d3
DangT <haidangc23hn2@gmail.com>
2021-06-21 15:55:36 +0700

    [f] 933 - View NFT details
    [178599933]https://www.pivotaltracker.com/story/show/178599933

--- src/options/content/bigNFTCard/index.js
+++ src/options/content/bigNFTCard/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-import KoiIcon from 'img/koi-logo.svg'
-import Button from 'popup/components/shared/button'
+import ArweaveIcon from 'img/arweave-icon.svg'
+import EmailIcon from 'img/social-icons/email-icon.svg'
+import FacebookIcon from 'img/social-icons/facebook-icon.svg'
+import LinkedinIcon from 'img/social-icons/linkedin-icon.svg'
+import TwitterIcon from 'img/social-icons/twitter-icon.svg'
 
 import './index.css'
 import { formatNumber } from '../../utils'
@@ -24,15 +27,18 @@ export default ({
     setTimeout(() => setIsCopied(false), 3000)
   }
 
+  const { registeredDate, description, tags } = {
+    registeredDate: 'June 7, 2021',
+    description:
+      'It is a long established fact that a reader will long established fact that a reader will long established fact that a reader will It is a long established fact that a reader will It is a long established fact that a reader willIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using It is a long established fact that a reader will long established fact that a reader will long established fact that a reader will It is a long established fact that a reader will It is a long established fact that a reader willIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using',
+    tags: ['crypto', 'puppies', 'electropop', 'cubism'],
+  }
+
   return (
     <div className='big-nft-card-wrapper' ref={bigCardRef}>
       <div className='big-nft-card'>
         {contentType.includes('image') ? (
-          <img
-            src={imageUrl}
-            className='nft-img'
-            onClick={() => setChoosen('')}
-          />
+          <img src={imageUrl} className='nft-img' />
         ) : (
           <video
             width={320}
@@ -43,32 +49,47 @@ export default ({
             autoPlay
           />
         )}
-        <div className='nft-name'>{name}</div>
-        {isRegistered ? (
-          <div className='nft-earned-koi'>{formatNumber(earnedKoi)} KOI</div>
-        ) : (
-          <button className='register-button'>
-            <KoiIcon className='icon' /> Register &amp; Earn
-          </button>
-        )}
-        {isRegistered && (
-          <>
-            <CopyToClipboard text={koiRockUrl}>
-              <>
-                {isCopied && <div className='copy-noti'>Link copied!</div>}
-                <Button
-                  label='Share'
-                  type='outline'
-                  className='share-nft-button'
-                  onClick={onCopy}
-                />
-              </>
-            </CopyToClipboard>
-            <a target='_blank' href={koiRockUrl} className='nft-path'>
-              View on koi.rocks
+        <div className='info'>
+          <div className='nft-name'>{name}</div>
+          <div className='export-nft'>
+            <ArweaveIcon className='arweave-icon' />
+            Export this NFT to a&nbsp;
+            <span className='different-chain'>different chain</span>.
+          </div>
+          <div className='registered-date'>Registered: {registeredDate}</div>
+          <div className='external-links'>
+            <a
+              className='external-link'
+              href='https://viewblock.io/'
+              target='_blank'
+            >
+              explore block
             </a>
-          </>
-        )}
+            <a className='external-link' href={koiRockUrl} target='_blank'>
+              koi.rocks
+            </a>
+          </div>
+          <div className='description'>{description}</div>
+          <div className='tags'>
+            {tags.map((tag) => (
+              <div className='tag-item'>{tag}</div>
+            ))}
+          </div>
+          <div className='earned'>
+            <div className='views'>0 views</div>
+            <div className='koi '>{formatNumber(earnedKoi)} KOI earned</div>
+          </div>
+          <div className='share-embed'>
+            <button className='share-button'>Share</button>
+            <button className='embed-button'>Embed</button>
+          </div>
+          <div className='social-icons'>
+            <TwitterIcon className='social-icon' />
+            <FacebookIcon className='social-icon' />
+            <LinkedinIcon className='social-icon' />
+            <EmailIcon className='social-icon' />
+          </div>
+        </div>
       </div>
     </div>
   )
