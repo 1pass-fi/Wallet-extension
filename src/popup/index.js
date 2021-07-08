791a633335713753bb8ad7e2d5e2de293498c2ba
Minh Vu <minh.vu@eastagile.com>
2021-07-08 17:36:48 +0700

    [b] - fix UI bugs when use finnie with a second monitor

--- src/popup/index.js
+++ src/popup/index.js
@@ -11,6 +11,45 @@ import store from './store'
 
 browser.runtime.sendMessage({ data: 'hello' })
 
+/**
+ * Temporary workaround for secondary monitors on MacOS where redraws don't happen
+ * @See https://bugs.chromium.org/p/chromium/issues/detail?id=971701
+ * 
+ * Copied from: https://stackoverflow.com/questions/56500742/why-is-my-google-chrome-extensions-popup-ui-laggy-on-external-monitors-but-not
+ */
+if (
+  // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+  window.screenLeft < 0 ||
+  window.screenTop < 0 ||
+  window.screenLeft > window.screen.width ||
+  window.screenTop > window.screen.height
+) {
+  chrome.runtime.getPlatformInfo(function (info) {
+    if (info.os === 'mac') {
+      const fontFaceSheet = new CSSStyleSheet()
+      fontFaceSheet.insertRule(`
+        @keyframes redraw {
+          0% {
+            opacity: 1;
+          }
+          100% {
+            opacity: .99;
+          }
+        }
+      `)
+      fontFaceSheet.insertRule(`
+        html {
+          animation: redraw 1s linear infinite;
+        }
+      `)
+      document.adoptedStyleSheets = [
+        ...document.adoptedStyleSheets,
+        fontFaceSheet,
+      ]
+    }
+  })
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
