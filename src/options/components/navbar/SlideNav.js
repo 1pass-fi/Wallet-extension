1030b6d8f366ff7a2e983520f92716c239100f02
Minh Vu <minh.vu@eastagile.com>
2021-07-12 16:36:40 +0700

    [f] - slide navbar

---
+++ src/options/components/navbar/SlideNav.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './SlideNav.css'
+
+import { useLocation } from 'react-router'
+import { Link } from 'react-router-dom'
+
+export default () => {
+  const { pathname } = useLocation()
+  return (
+    <div className='slide-nav'>
+      <div className='items'>
+        <Link style={{ textDecoration: 'none' }} to='/settings/k-id'>
+          <div className={pathname == '/settings/k-id' ? 'item active' : 'item'}>Koi Identity (kID)</div>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='/settings/gallery'>
+          <div className={pathname == '/settings/gallery' ? 'item active' : 'item'}>Gallery Settings</div>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='/settings/wallet'>
+          <div className={pathname == '/settings/wallet' ? 'item active' : 'item'}>Wallet Settings</div>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='/settings/security'>
+          <div className={pathname == '/settings/security' ? 'item active' : 'item'}>Security</div>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='#'>
+          <div className={pathname == '/settings/customization' ? 'item active' : 'item'}>Customization</div>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to='/about'>
+          <div className={pathname == '/about' ? 'item active' : 'item'}>About</div>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- src/options/components/navbar/index.js
+++ src/options/components/navbar/index.js
@@ -12,42 +12,47 @@ import { Link } from 'react-router-dom'
 
 import { GalleryContext } from 'options/galleryContext'
 
+import SlideNavbar from './SlideNav'
+
 export default () => {
   const { affiliateCode } = useContext(GalleryContext)
   const { pathname } = useLocation()
 
   return (
-    <div className='navbar'>
-      <div className='top'>
-        <Link to='/'>
-          <HomeIcon className={`nav-item ${pathname == '/' ? 'active' : ''}`} />
-        </Link>
-        <Link to='/create'>
-          <CreateNFTIcon
-            className={`nav-item ${pathname == '/create' ? 'active' : ''}`}
-          />
-        </Link>
-        <Link to='/collections'>
-          <CollectionIcon
-            className={`nav-item ${pathname == '/collections' ? 'active' : ''}`}
-          />
-        </Link>
-        <Link to='/settings'>
-          <SettingIcon
-            className={`nav-item ${pathname == '/settings' ? 'active' : ''}`}
-          />
-        </Link>
+    <div className='navbar-container'>
+      <div className='navbar'>
+        <div className='top'>
+          <Link to='/'>
+            <HomeIcon className={`nav-item ${pathname == '/' ? 'active' : ''}`} />
+          </Link>
+          <Link to='/create'>
+            <CreateNFTIcon
+              className={`nav-item ${pathname == '/create' ? 'active' : ''}`}
+            />
+          </Link>
+          <Link to='/collections'>
+            <CollectionIcon
+              className={`nav-item ${pathname == '/collections' ? 'active' : ''}`}
+            />
+          </Link>
+          <Link to='/settings'>
+            <SettingIcon
+              className={`nav-item ${pathname == '/settings' ? 'active' : ''}`}
+            />
+          </Link>
+        </div>
+        <div className='bottom'>
+          <Link to='/friends'>
+            <FriendIcon
+              className={`nav-item friend ${
+                pathname == '/friends' ? 'active' : ''
+              }`}
+            />
+          </Link>
+          <div className='address'>{`${affiliateCode && affiliateCode.slice(0,5)}...`}</div>
+        </div>
       </div>
-      <div className='bottom'>
-        <Link to='/friends'>
-          <FriendIcon
-            className={`nav-item friend ${
-              pathname == '/friends' ? 'active' : ''
-            }`}
-          />
-        </Link>
-        <div className='address'>{`${affiliateCode && affiliateCode.slice(0,5)}...`}</div>
-      </div>
+      <SlideNavbar />
     </div>
   )
 }
