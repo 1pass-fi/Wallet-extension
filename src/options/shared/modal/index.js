c3116e56d6fa5dd2ec80cdd7d506076c57e3969d
DangT <haidangc23hn2@gmail.com>
2021-07-15 16:53:00 +0700

    [c] click outside effect

--- src/options/shared/modal/index.js
+++ src/options/shared/modal/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import CloseIcon from 'img/circle-close-icon.svg'
 
@@ -9,10 +9,25 @@ const propTypes = {
 }
 
 const Modal = ({ onClose, children, className = '' }) => {
+  const ref = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClose()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref])
+
   return (
-    <div className="modal-container">
-      <div className={`modal ${className}`}>
-        <div className="modal-close-icon">
+    <div className='modal-container'>
+      <div className={`modal ${className}`} ref={ref}>
+        <div className='modal-close-icon'>
           <CloseIcon onClick={onClose} />
         </div>
         {children}
