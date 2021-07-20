aaa5c947f6396ae233c32489bd7c08d5ccb0e041
DangT <haidangc23hn2@gmail.com>
2021-07-20 10:45:29 +0700

    178 - select wallet modal

--- src/options/modal/exportNFT/index.js
+++ src/options/modal/exportNFT/index.js
@@ -9,14 +9,13 @@ import Avalanche from 'img/chain/avalanche-logo.svg'
 import PolkadotLogo from 'img/chain/polkadot-logo.svg'
 import TezosLogo from 'img/chain/tezos-logo.svg'
 
-const Chain = ({Icon, title}) => {
+const Chain = ({ Icon, title }) => {
   return (
     <div className='export-nft chain container'>
-      <div className='export-nft chain icon'><Icon /></div>
-      <div className='export-nft chain text'>
-        {title}
-        <button className='export-nft chain button'>coming soon!</button>
+      <div className='export-nft chain icon'>
+        <Icon />
       </div>
+      <div className='export-nft chain text'>{title}</div>
     </div>
   )
 }
@@ -26,14 +25,24 @@ const ExportNFT = () => {
     <div className='export-nft container'>
       <div className='export-nft title container'>
         Share to earn Attention Rewards
-        <div className='export-nft title description'>List your NFT on any exchange or display it in a gallery on any chain, without making a duplicate.</div>
+        <div className='export-nft title description'>
+          List your NFT on any exchange or display it in a gallery on any chain,
+          without making a duplicate.
+        </div>
       </div>
+
+      <div className='ethereum'>
+        <EthereumLogo className='logo' />
+        <div className='text'>Ethereum</div>
+      </div>
+
+      <div className='coming-soon'>Coming soon:</div>
+
       <div className='export-nft chains container'>
-        <Chain Icon={EthereumLogo} title='Ethereum'/>
-        <Chain Icon={TezosLogo} title='Tezos'/>
-        <Chain Icon={PolkadotLogo} title='Polkadot'/>
-        <Chain Icon={BinanceLogo} title='Binance'/>
-        <Chain Icon={Avalanche} title='Avalanche'/>
+        <Chain Icon={TezosLogo} title='Tezos' />
+        <Chain Icon={PolkadotLogo} title='Polkadot' />
+        <Chain Icon={BinanceLogo} title='Binance' />
+        <Chain Icon={Avalanche} title='Avalanche' />
       </div>
     </div>
   )
@@ -42,7 +51,9 @@ const ExportNFT = () => {
 export default ({ onClose }) => {
   return (
     <div>
-      <Modal onClose={onClose}><ExportNFT /></Modal>
+      <Modal onClose={onClose}>
+        <ExportNFT />
+      </Modal>
     </div>
   )
 }
