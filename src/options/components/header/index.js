c2bcb51ac79960dc35df7b19010c8b75d0635f0c
DangT <haidangc23hn2@gmail.com>
2021-08-05 15:47:25 +0700

    [c] 058  - Change loading UI
    [#179129058] https://www.pivotaltracker.com/story/show/179129058

--- src/options/components/header/index.js
+++ src/options/components/header/index.js
@@ -6,13 +6,14 @@ import KoiIcon from 'img/finnie-koi-logo-white.svg'
 import ArUnit from 'img/ar-token.svg'
 import KoiUnit from 'img/koi-token.svg'
 import SearchBar from './SearchBar'
+import Loading from 'options/components/loading'
 
 import { formatNumber } from '../../utils'
 
 import './index.css'
 import { GalleryContext } from 'options/galleryContext'
 
-export default ({ totalKoi, totalAr, headerRef }) => {
+export default ({ totalKoi, totalAr, headerRef, isLoading }) => {
   const { wallets, setAccount } = useContext(GalleryContext)
   const { pathname } = useLocation()
 
@@ -36,21 +37,26 @@ export default ({ totalKoi, totalAr, headerRef }) => {
       </div> */}
       <div className='header-center'>{pathname == '/' && <SearchBar />}</div>
       <div className='header-right'>
-        <div className='total-koi'>
-          <KoiUnit className='koi-unit' />
-          <div>{formatNumber(totalKoi)}</div>
-          <ArUnit className='ar-unit' />
-          <div>{formatNumber(totalAr, 6)}</div>
+        {isLoading && <Loading />}
+        <div className='koi-info'>
+          <div className='total-koi'>
+            <KoiUnit className='koi-unit' />
+            <div>{formatNumber(totalKoi)}</div>
+            <ArUnit className='ar-unit' />
+            <div>{formatNumber(totalAr, 6)}</div>
+          </div>
+          {!totalKoi && (
+            <a
+              target='_blank'
+              href='https://koi.rocks/faucet?step=0'
+              className='no-koi'
+            >
+              <div className='get-some'>
+                No KOII? <u>Get some</u>
+              </div>
+            </a>
+          )}
         </div>
-        {!totalKoi && 
-          <a
-            target='_blank'
-            href='https://koi.rocks/faucet?step=0'
-            className='no-koi'
-          >
-            <div className='get-some'>No KOII? <u>Get some</u></div>
-          </a> 
-        }
       </div>
     </header>
   )
