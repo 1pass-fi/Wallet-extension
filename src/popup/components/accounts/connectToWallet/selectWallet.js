3df9774d736ed85f5a6907cae8e2d336ad405df9
DangT <haidangc23hn2@gmail.com>
2021-08-06 15:06:04 +0700

    [f] connect site multiple account

--- src/popup/components/accounts/connectToWallet/selectWallet.js
+++ src/popup/components/accounts/connectToWallet/selectWallet.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import includes from 'lodash/includes'
+import isEmpty from 'lodash/isEmpty'
 
 import KoiIcon from 'img/koi-logo.svg'
 import ArweaveIcon from 'img/arweave-icon.svg'
@@ -13,7 +14,6 @@ const walletIcon = {
   arweave: <ArweaveIcon className='wallet-icon' />,
 }
 
-
 const SelectWallet = ({
   accounts,
   checkedList,
@@ -21,34 +21,33 @@ const SelectWallet = ({
   checkAll,
   onChecked,
   setStep,
-  handleOnClick
+  handleOnClick,
 }) => {
   return (
     <>
       <div className='select-account'>Select accounts</div>
-      {
-        accounts.lenght > 1 && (
-          <div>
-            <button className='unselect-button' onClick={clearChecked}>
-              -
-            </button>
-            <button className='select-all-button' onClick={checkAll}>
-              Select all
-            </button>
-          </div>
-        )
-      }
+      {accounts.lenght > 1 && (
+        <div>
+          <button className='unselect-button' onClick={clearChecked}>
+            -
+          </button>
+          <button className='select-all-button' onClick={checkAll}>
+            Select all
+          </button>
+        </div>
+      )}
       <div className='wallet-options'>
         {accounts.map((account) => (
           <div
-            className={`wallet-option ${includes(checkedList, account.address) && 'checked'
+            className={`wallet-option ${
+              includes(checkedList, account.address) && 'checked'
             }`}
             key={account.address}
           >
-            <Checkbox 
-              defaultChecked={true}
+            <Checkbox
+              defaultChecked={false}
               className='check-wallet'
-              isDisabled={true}
+              isDisabled={false}
               onChange={(e) => onChecked(e, account.address)}
             />
             {walletIcon[account.type]}
@@ -65,8 +64,18 @@ const SelectWallet = ({
       </div>
       <div className='trust-sites'>Only connect with sites you trust.</div>
       <div className='button-line'>
-        <Button className='button connect' label='Connect' onClick={() => setStep(2)} />
-        <Button className='button reject' type='outline' label='Reject' onClick={() => handleOnClick(false)} />
+        <Button
+          className='button connect'
+          label='Connect'
+          onClick={() => setStep(2)}
+          isEnable={!isEmpty(checkedList)}
+        />
+        <Button
+          className='button reject'
+          type='outline'
+          label='Reject'
+          onClick={() => handleOnClick(false)}
+        />
       </div>
     </>
   )
