fb97e175e497e72fefdfd20488f6f8240ff7e222
Abel <abuga8@yahoo.com>
2021-09-03 03:39:01 +0200

    fix

--- src/utils/bridge/bridge.js
+++ src/utils/bridge/bridge.js
@@ -15,24 +15,25 @@ const fromEthToArweave = async () => {
     const addresses = await web3.eth.getAccounts()
     console.log('addresses: ', addresses)
     /* 
-      create contract from koiToken contract address
+      create contract from koiToken/user nft contract address, it can be koiNFt contract or any other nfts contract
     */
-    const koiTokenContract = new web3.eth.Contract(ABI,'0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC')
+    const nftContract = new web3.eth.Contract(ABI,'0xff3096ED566445c9F24F615b3afD6677AD4Dcba4')
     
     /* 
-      create contract from koiRouter contract address
+      create contract from koiRouter contract address 0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC
     */
-    const koiRouterContract = new web3.eth.Contract(ABI2, '0xff3096ED566445c9F24F615b3afD6677AD4Dcba4')
+    const koiRouterContract = new web3.eth.Contract(ABI2, '0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC')
 
-    const isApprove = await koiRouterContract.methods.isApprovedForAll(addresses[0], '0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC').call()
+    const isApprove = await nftContract.methods.isApprovedForAll(addresses[0], '0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC').call()
     console.log('isApprove', isApprove)
 
     if (isApprove === false) {
-      const result = await koiRouterContract.methods.setApprovalForAll('0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC', true).send({from: addresses[0]})
+      // give router contract acces to make transaction on behalf of the user
+      const result = await nftContract.methods.setApprovalForAll('0xD1183ad3B7934466aCB98D17B85Ced15999EA3AC', true).send({from: addresses[0]})
       console.log('*****RECEIPT1: ', result)
     }
 
-    const depositResult = await koiTokenContract.methods.deposit('0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656', '79815999319843347607878690319208729376727520789855333437166829176352736804865', 1, 'ou-OUmrWuT0hnSiUMoyhGEbd3s5b_ce8QK0vhNwmno4').send({from: addresses[0]})
+    const depositResult = await nftContrat.methods.deposit('0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656', '79815999319843347607878690319208729376727520789855333437166829176352736804865', 1, 'ou-OUmrWuT0hnSiUMoyhGEbd3s5b_ce8QK0vhNwmno4').send({from: addresses[0]})
     console.log('*****RECEIPT2: ', depositResult)
   } catch (err) {
     console.log(err.message)
