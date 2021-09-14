a8fbf5e50b6ddec1bb4c3e2b160c093431644959
thuan.ngo <thuan.ngo@eastagile.com>
2021-09-14 17:22:57 +0700

    [f] 901 - Add multiple account filter for popup assets tab
    [#179570901] https://www.pivotaltracker.com/story/show/179570901

--- src/popup/components/assets/index.js
+++ src/popup/components/assets/index.js
@@ -1,10 +1,9 @@
 // modules
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { connect } from 'react-redux'
 
 // actions
 import { loadContent } from 'actions/koi'
-import { setAssets } from 'actions/assets'
 import { setContLoading } from 'actions/continueLoading'
 import { setError } from 'actions/error'
 import { setNotification } from 'popup/actions/notification'
@@ -14,72 +13,133 @@ import { NOTIFICATION } from 'constants/koiConstants'
 
 // components
 import AssetList from './AssetList'
+import CheckBox from 'shared/checkbox'
+import ToggleButton from 'shared/ToggleButton'
 
-// services
-import { popupAccount } from 'services/account'
+// assets
+import CollapseIcon from 'img/collapse-icon.svg'
+import ExtendIcon from 'img/extend-icon.svg'
 
+// styles
+import './index.css'
 
-const Assets = ({ 
-  assets, 
-  setAssets, 
-  loadContent, 
-  isContLoading, 
-  setContLoading, 
-  setError, 
-  setNotification }) => {
+const Assets = ({
+  accounts,
+  assets,
+  loadContent,
+  setContLoading,
+  setError,
+  setNotification,
+  settings,
+}) => {
+  const [showAllAccounts, setShowAllAccounts] = useState(false)
+  const [selectAccountsCollapsed, setSelectAccountCollapsed] = useState(true)
+  const [accountsToShow, setAccountsToShow] = useState([])
+  const [filteredAssets, setFilteredAssets] = useState([])
 
   useEffect(() => {
-    async function handleLoadContent() {
-      // load from local storage
-      const allAccounts = await popupAccount.getAllAccounts()
-      const allAssets = []
-
-      await Promise.all(allAccounts.map(async account => {
-        const assets = await account.get.assets() || []
-        const address = await account.get.address()
-
-        const accountAssets = { owner: address, contents: assets }
-        allAssets.push(accountAssets)
-      }))
-
-      setAssets(allAssets)
-      
-
+    const handleLoadContent = async () => {
       // fetch data
-      if (!isContLoading) {
-        try {
-          setContLoading(true)
-          const allNftLoaded = await loadContent()
-          setContLoading(false)
+      try {
+        setContLoading(true)
+        const allNftLoaded = await loadContent()
+        setContLoading(false)
 
-          if (allNftLoaded) setNotification(NOTIFICATION.NFT_LOADED)
-        } catch (err) {
-          setContLoading(false)
-          setError(err.message)
-        }
+        if (allNftLoaded) setNotification(NOTIFICATION.NFT_LOADED)
+      } catch (err) {
+        setContLoading(false)
+        setError(err.message)
       }
     }
+
     handleLoadContent()
   }, [])
+
+  useEffect(() => {
+    let showAssets = []
+    if (showAllAccounts) {
+      showAssets = assets
+    } else {
+      showAssets = assets.filter((asset) =>
+        accountsToShow.includes(asset.owner)
+      )
+    }
+
+    setFilteredAssets(showAssets)
+  }, [accountsToShow, showAllAccounts, assets])
+
+  const handleSelectAccount = (e) => {
+    const selectedAddress = e.target.id
+    if (accountsToShow.includes(selectedAddress)) {
+      setAccountsToShow(
+        accountsToShow.filter((address) => address !== selectedAddress)
+      )
+    } else {
+      setAccountsToShow([...accountsToShow, selectedAddress])
+    }
+  }
 
   const onAddAsset = () => {
     const url = chrome.extension.getURL('options.html#/create')
     chrome.tabs.create({ url })
   }
+
   return (
-    <div>
-      {
-        assets.map((asset, index) => <AssetList 
-          owner={asset.owner} 
-          assets={asset.contents || []} 
-          onAddAsset={onAddAsset} 
+    <div className="assets-tab">
+      <div className="assets-setting">
+        All Accounts
+        <ToggleButton value={!showAllAccounts} setValue={setShowAllAccounts} />
+        Individual
+        {!showAllAccounts && (
+          <div
+            onClick={() => setSelectAccountCollapsed(!selectAccountsCollapsed)}
+            className="collapse-extend-icon"
+          >
+            {selectAccountsCollapsed ? (
+              <ExtendIcon data-tip="Expand" />
+            ) : (
+              <CollapseIcon data-tip="Collapse" />
+            )}
+          </div>
+        )}
+      </div>
+
+      {!showAllAccounts && !selectAccountsCollapsed && (
+        <div className="select-accounts">
+          {accounts.map((account) => (
+            <div key={account.address} className="account">
+              <CheckBox
+                id={account.address}
+                onChange={handleSelectAccount}
+                defaultChecked={accountsToShow.includes(account.address)}
+              />
+              <label htmlFor={account.address}>{account.accountName}</label>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {filteredAssets.map((asset, index) => (
+        <AssetList
+          showAccountName={!showAllAccounts}
+          owner={asset.owner}
+          assets={asset.contents || []}
+          onAddAsset={onAddAsset}
           key={index}
-        />)
-      }
+        />
+      ))}
     </div>
   )
 }
 
-const mapStateToProps = (state) => ({ assets: state.assets, isContLoading: state.contLoading })
+const mapStateToProps = (state) => ({
+  assets: state.assets,
+  accounts: state.accounts,
+})
 
-export default connect(mapStateToProps, { loadContent, setAssets, setContLoading, setError, setNotification })(Assets)
+export default connect(mapStateToProps, {
+  loadContent,
+  setContLoading,
+  setError,
+  setNotification,
+})(Assets)
