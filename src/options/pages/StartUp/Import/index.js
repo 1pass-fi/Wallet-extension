150178fd531aa0c61a808f0b794283cbb5424afe
thuan.ngo <thuan.ngo@eastagile.com>
2021-10-08 21:58:32 +0700

    [b] - Handle empty seedphrase

--- src/options/pages/StartUp/Import/index.js
+++ src/options/pages/StartUp/Import/index.js
@@ -161,6 +161,7 @@ export default () => {
                }
 
               <Button
+                disabled={isEmpty(userSeedPhrase)}
                 className='import-key-button'
                 onClick={onImportKey}
               >
