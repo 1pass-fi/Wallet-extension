2dbd8f30e49c82af50bdc932e48c3abf0dd33cad
thuan.ngo <thuan.ngo@eastagile.com>
2021-10-20 16:24:34 +0700

    [f] 156 - Generic button component for address book
    [#180010156] https://www.pivotaltracker.com/story/show/180010156

---
+++ src/options/components/AddressBook/Button/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+
+import './index.css'
+
+const variantClassName = {
+  normal: 'text-green',
+  delete: 'big text-red',
+  cancel: 'big text-white',
+}
+
+const Button = ({
+  startIcon: StartIcon = EditIcon,
+  text,
+  variant,
+  isLoading = false,
+  ...props
+}) => {
+  return (
+    <button
+      className={clsx('addressBookBtn', variantClassName[variant])}
+      disabled={isLoading}
+      {...props}
+    >
+      <div className="startIcon">
+        <StartIcon />
+      </div>
+      {text}
+    </button>
+  )
+}
+
+Button.PropTypes = {
+  startIcon: PropTypes.elementType,
+  text: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf('normal', 'delete', 'cancel').isRequired,
+  isLoading: PropTypes.bool,
+}
+
+export default Button
