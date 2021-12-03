6e962d577093cfa669724b9664ef526434ec53d8
thuan.ngo <thuan.ngo@eastagile.com>
2021-12-03 16:33:42 +0700

    [c] 297 - Initialize sub module for finnie redesign
    [#180518297] https://www.pivotaltracker.com/story/show/180518297
    
    Co-authored-by: LongP <long.pham@eastagile.com>

---
+++ src/finnie-v2/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style.css'
+
+const SecondVer = () => {
+  return <div className="font-bold text-blue-500 text-lg">Go To The Gallery</div>
+}
+
+export default SecondVer
--- src/options/index.js
+++ src/options/index.js
@@ -18,6 +18,8 @@ import UploadWallet from './pages/StartUp/Upload'
 import ImportWallet from './pages/StartUp/Import'
 import CreateWallet from './pages/StartUp/Create'
 
+import SecondVer from '../finnie-v2'
+
 import Success from './pages/StartUp/shared/Success'
 import HasArweave from 'options/shared/hasArweave'
 
@@ -27,58 +29,64 @@ const Gallery = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Layout>
-          <Switch>
-            <Route exact path="/welcome">
-              <Welcome />
-            </Route>
-            <Route exact path="/create-wallet">
-              <CreateWallet />
-            </Route>
-            <Route exact path="/upload-wallet">
-              <UploadWallet />
-            </Route>
-            <Route exact path="/import-wallet">
-              <ImportWallet />
-            </Route>
-            <Route exact path="/create">
-              <HasArweave content="This feature only supports AR wallets">
-                <Create />
-              </HasArweave>
-            </Route>
-            <Route exact path="/faucet">
-              <Faucet />
-            </Route>
-            <Route exact path="/friends">
-              <HasArweave
-                content="This feature only supports AR wallets"
-                checkingDefaultAccount={true}
-              >
-                <Friends />
-              </HasArweave>
-            </Route>
-            <Route exact path="/collections/:collectionId">
-              <CollectionDetails />
-            </Route>
-            <Route exact path="/collections">
-              <HasArweave content="This feature only supports AR wallets">
-                <Collections />
-              </HasArweave>
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-            <Route path="/success">
-              <Success />
-            </Route>
-            <Route path="*">
-              <Home />
-            </Route>
-          </Switch>
-          <Route exact path="/details/:txid">
-            <Details />
+        <Switch>
+          <Route exact path="/v2">
+            <SecondVer />
           </Route>
-        </Layout>
+          <Layout>
+            <Switch>
+              <Route exact path="/welcome">
+                <Welcome />
+              </Route>
+              <Route exact path="/create-wallet">
+                <CreateWallet />
+              </Route>
+              <Route exact path="/upload-wallet">
+                <UploadWallet />
+              </Route>
+              <Route exact path="/import-wallet">
+                <ImportWallet />
+              </Route>
+              <Route exact path="/create">
+                <HasArweave content="This feature only supports AR wallets">
+                  <Create />
+                </HasArweave>
+              </Route>
+              <Route exact path="/faucet">
+                <Faucet />
+              </Route>
+              <Route exact path="/friends">
+                <HasArweave
+                  content="This feature only supports AR wallets"
+                  checkingDefaultAccount={true}
+                >
+                  <Friends />
+                </HasArweave>
+              </Route>
+              <Route exact path="/collections/:collectionId">
+                <CollectionDetails />
+              </Route>
+              <Route exact path="/collections">
+                <HasArweave content="This feature only supports AR wallets">
+                  <Collections />
+                </HasArweave>
+              </Route>
+              <Route path="/settings">
+                <Settings />
+              </Route>
+              <Route path="/success">
+                <Success />
+              </Route>
+              <Route path="*">
+                <Home />
+              </Route>
+            </Switch>
+
+            <Route exact path="/details/:txid">
+              <Details />
+            </Route>
+          </Layout>
+        </Switch>
       </Router>
     </Provider>
   )
