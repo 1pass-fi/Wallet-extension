e539be50ab3829cffcab1c6d04b7625298207d03
Long Pham <long.pham@eastagile.com>
2021-12-06 13:34:53 +0700

    [f] 370 - Show AR/KOII balances
    [#180518370] https://www.pivotaltracker.com/story/show/180518370
    
    Co-authored-by: ThuanN <thuan.ngo@eastagile.com>

--- src/finnie-v2/index.js
+++ src/finnie-v2/index.js
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useStore } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
-
-import get from 'lodash/get'
 
 import { setDefaultAccount } from 'options/actions/defaultAccount'
 
-import { MESSAGES } from 'constants/koiConstants'
-
 import { popupAccount } from 'services/account'
-import { popupBackgroundConnect } from 'services/request/popup'
-import { EventHandler } from 'services/request/src/backgroundConnect'
 import storage from 'services/storage'
 
 import Gallery from './pages/Gallery'
@@ -18,59 +12,20 @@ import Gallery from './pages/Gallery'
 import './style.css'
 
 const SecondVer = () => {
-  const store = useStore()
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    const updateAccounts = async () => {
-      await popupAccount.loadImported()
+  const updateDefaultAccountData = async () => {
+    const activatedAccountAddress = await storage.setting.get.activatedAccountAddress()
+    const activatedAccount = await popupAccount.getAccount({
+      address: activatedAccountAddress
+    })
+    const activatedAccountMetadata = await activatedAccount.get.metadata()
 
-      /* 
-        Set default account if imported account is the first account
-      */
-      if (get(popupAccount, 'importedAccount.length') === 1) {
-        let activatedAccount = await storage.setting.get.activatedAccountAddress()
-        activatedAccount = await popupAccount.getAccount({
-          address: activatedAccount
-        })
-        activatedAccount = await activatedAccount.get.metadata()
-        dispatch(setDefaultAccount(activatedAccount))
-      }
-    }
-
-    updateAccounts()
-  }, [])
+    dispatch(setDefaultAccount(activatedAccountMetadata))
+  }
 
   useEffect(() => {
-    const handleAddHandler = () => {
-      const loadBalancesSuccess = new EventHandler(MESSAGES.GET_BALANCES_SUCCESS, async () => {
-        try {
-          const { defaultAccount } = store.getState()
-
-          let activatedAccount = await storage.setting.get.activatedAccountAddress()
-          activatedAccount = await popupAccount.getAccount({
-            address: activatedAccount
-          })
-          const activatedAccountData = await activatedAccount.get.metadata()
-          const { balance, koiBalance } = activatedAccountData
-
-          const balancesUpdated =
-            defaultAccount.balance !== balance || defaultAccount.koiBalance !== koiBalance
-          if (balancesUpdated) {
-            await dispatch(loadAllAccounts())
-
-            setNotification('Your balances have been updated.')
-          }
-
-        } catch (err) {
-          setError(err.message)
-        }
-      })
-
-      popupBackgroundConnect.addHandler(loadBalancesSuccess)
-    }
-
-    handleAddHandler()
+    updateDefaultAccountData()
   }, [])
 
   return (
