52de4219bf0e54b6db63a680303a06bda82a9ed5
thuan.ngo <thuan.ngo@eastagile.com>
2021-12-22 15:22:43 +0700

    [b] 730 - Make nft grid responsive
    [#180706730] https://www.pivotaltracker.com/story/show/180706730

--- src/finnie-v2/components/MainLayout/MainLayout.js
+++ src/finnie-v2/components/MainLayout/MainLayout.js
@@ -8,12 +8,12 @@ const MainLayout = ({ title, children }) => {
     <div className="w-full min-h-screen h-full bg-gradient-to-r from-blueGray-900 to-indigo via-indigo-800">
       <NavBar />
       <div className="w-full z-40 fixed top-16 h-18.75 bg-gradient-to-r from-blueGray-900 to-indigo via-indigo-800" />
-      <div className="w-full lg:w-11/12 xl:w-2/3 mx-auto">
+      <div className="w-full lg:w-11/12 2xl:w-2/3 mx-auto">
         <div className="sticky top-16 z-40 bg-transparent text-white w-full text-3xl tracking-wider h-18.75 px-13.75 pt-7.25">
           {title.toUpperCase()}
         </div>
         <div className="pl-4.25">
-          <aside className="fixed z-51 w-57.75 pt-4">
+          <aside className="fixed z-51 w-61 pt-4">
             <Sidebar />
           </aside>
           <main className="ml-65.5 pb-5">{children}</main>
