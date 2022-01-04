96356d6b169b8777c618d18384f832c530750992
Minh Vu <minh.vu@EAs-iMac.local>
2022-01-04 15:24:49 +0700

    [f] 221 Finnie code injection options

--- src/content_scripts/inject.js
+++ src/content_scripts/inject.js
@@ -1,12 +1,16 @@
 import { MESSAGES } from 'constants/koiConstants'
+import storage from 'services/storage'
 
-export default (fn) => {
+export default async (fn) => {
   const script = document.createElement('script')
   const arweaveScript = document.createElement('script')
 
   script.text = `const MESSAGE_TYPES = JSON.parse('${JSON.stringify(MESSAGES)}');(${fn.toString()})();`
   arweaveScript.src = 'https://unpkg.com/arweave/bundles/web.bundle.js'
 
-  document.documentElement.appendChild(arweaveScript)
-  document.documentElement.appendChild(script)
+  const disabledOrigins = await storage.setting.get.disabledOrigins()
+  const origin = window.location.origin
+
+  if (!disabledOrigins.includes(origin)) document.documentElement.appendChild(arweaveScript)
+  if (!disabledOrigins.includes(origin)) document.documentElement.appendChild(script)
 }
