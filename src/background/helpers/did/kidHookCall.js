af4bb14245c82d07a35b44b4b862ac81c51042c8
Minh Vu <minh.vu@EAs-iMac.local>
2022-01-14 16:53:19 +0700

    [f] 766 - 0.2.10 build - call hook on updated did

--- src/background/helpers/did/kidHookCall.js
+++ src/background/helpers/did/kidHookCall.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default async (txId) => {
-  const BASE_URL = 'https://api.koii.live/generateCard'
+  const BASE_URL = 'https://api.koii.live/generateDID'
   return axios.get(`${BASE_URL}/${txId}`)
 }
