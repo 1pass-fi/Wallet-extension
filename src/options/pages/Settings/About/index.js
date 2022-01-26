cb71951eb160c1a2d46f36367ee8fb159baaef6d
thuan.ngo <thuan.ngo@eastagile.com>
2022-01-26 16:12:03 +0700

    [f] 039 - Settings about page
    [#181042039] https://www.pivotaltracker.com/story/show/181042039

--- src/options/pages/Settings/About/index.js
+++ src/options/pages/Settings/About/index.js
@@ -7,91 +7,67 @@ export default ({ version = '0.2.10' }) => {
   const { setShowWelcome } = useContext(GalleryContext)
 
   return (
-    <div className='about-settings-wrapper'>
-      <div className='about-settings'>
-        <div className='header'>About Finnie</div>
+    <div className="about-settings-wrapper">
+      <div className="about-settings">
+        <div className="header">About</div>
 
-        <div className='items'>
-          <div className='item'>
-            <div className='title'>Version Notes</div>
-            <div className='description'>
-              See the details about the latest release.
+        <div className="items">
+          <div className="item version-notes">
+            <div>
+              <div className="title">Version Notes</div>
+              <div className="description">See the details about the latest release.</div>
             </div>
-            <div className='version-note'>
-              <button onClick={() => setShowWelcome(true)}>Version &nbsp;{version}&nbsp; Notes</button>
+            <div className="version-note">
+              <button onClick={() => setShowWelcome(true)}>
+                Version &nbsp;{version}&nbsp; Notes
+              </button>
             </div>
           </div>
 
-          <div className='item'>
-            <div className='title'>Privacy Policy</div>
-            <div className='description'>
+          <div className="item">
+            <div className="title">Privacy Policy</div>
+            <div className="description">
               Find Koii’s &nbsp;
-              <a
-                href='https://koii.network/Privacy_Policy.html'
-                target='_blank'
-                className='link'
-              >
+              <a href="https://koii.network/Privacy_Policy.html" target="_blank" className="link">
                 Privacy Policy here.
               </a>
             </div>
           </div>
 
-          <div className='item'>
-            <div className='title'>Terms of Use</div>
-            <div className='description'>
+          <div className="item">
+            <div className="title">Terms of Use</div>
+            <div className="description">
               See Koii’s &nbsp;
-              <a
-                href='https://koii.network/TOU_June_22_2021.pdf'
-                target='_blank'
-                className='link'
-              >
+              <a href="https://koii.network/TOU_June_22_2021.pdf" target="_blank" className="link">
                 Terms of Use.
               </a>
             </div>
           </div>
 
-          <div className='item'>
-            <div className='title'>Privacy Policy</div>
-            <div className='description'>
+          <div className="item">
+            <div className="title">The KOII network</div>
+            <div className="description">
               Check out &nbsp;
-              <a href='https://koii.network/' target='_blank' className='link'>
+              <a href="https://koii.network/" target="_blank" className="link">
                 Koii’s website
               </a>
               , and the &nbsp;
-              <a href='https://koi.rocks/' target='_blank' className='link'>
+              <a href="https://koi.rocks/" target="_blank" className="link">
                 NFT leaderboard
               </a>
               .
             </div>
           </div>
 
-          <div className='item'>
-            <div className='title'>Need support?</div>
-            <div className='description'>
-              Email us at &nbsp;
-              <a
-                href='mailto:support@koii.network'
-                target='_blank'
-                className='link'
-              >
-                support@koii.network
+          <div className="item">
+            <div className="title">Need help?</div>
+            <div className="description">
+              Reach out to Koii's &nbsp;
+              <a href="mailto:support@koii.network" target="_blank" className="link">
+                support team
               </a>
-              .
-            </div>
-            <div className='report-request'>
-              <a
-                href='https://koii.me/support/'
-                className='link'
-                target='_blank'
-              >
-                Report
-              </a>
-              &nbsp;a bug or &nbsp;
-              <a
-                href='https://koii.me/featurerequest/'
-                className='link'
-                target='_blank'
-              >
+              &nbsp; or &nbsp;{' '}
+              <a href="https://koii.me/featurerequest/" className="link" target="_blank">
                 request
               </a>
               &nbsp; a feature.
