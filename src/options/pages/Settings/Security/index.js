e32a4bd87fbec93a19929352b9bd0fc1131d7eed
thuan.ngo <thuan.ngo@eastagile.com>
2022-01-26 17:05:48 +0700

    [r] - Re format code
    [#181042755] https://www.pivotaltracker.com/story/show/181042755

--- src/options/pages/Settings/Security/index.js
+++ src/options/pages/Settings/Security/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-
 import FinnieIcon from 'img/finnie-koi-logo-blue.svg'
 import EthereumIcon from 'img/ethereum-logo.svg'
 import { getDisplayAddress } from 'options/utils'
@@ -9,10 +8,7 @@ import { getChromeStorage } from 'utils'
 import { STORAGE } from 'constants/koiConstants'
 import { getArAccounts } from 'options/selectors/accounts'
 
-import {
-  ExportBackupPhraseModal,
-  ExportBackupKeyFileModal,
-} from './ExportModal'
+import { ExportBackupPhraseModal, ExportBackupKeyFileModal } from './ExportModal'
 
 import './index.css'
 import { TYPE } from 'constants/accountConstants'
@@ -21,18 +17,12 @@ export default () => {
   const [seedPhrase, setSeedPhrase] = useState('')
   const [hasSeedPhrase, setHasSeedPhrase] = useState(false)
 
-  const accounts = useSelector(state => state.accounts)
+  const accounts = useSelector((state) => state.accounts)
   const arAccounts = useSelector(getArAccounts)
 
-  const [
-    showExportBackupPhraseModal,
-    setShowExportBackupPhraseModal,
-  ] = useState(false)
+  const [showExportBackupPhraseModal, setShowExportBackupPhraseModal] = useState(false)
 
-  const [
-    showExportBackupKeyfileModal,
-    setShowExportBackupKeyfileModal,
-  ] = useState(false)
+  const [showExportBackupKeyfileModal, setShowExportBackupKeyfileModal] = useState(false)
 
   const [selectedAccount, setSelectedAccount] = useState()
 
@@ -44,7 +34,7 @@ export default () => {
 
     checkSeedPhrase()
   }, [])
-  
+
   const onSeedPhraseClick = (account) => {
     setSelectedAccount(account)
     setShowExportBackupPhraseModal(true)
@@ -62,65 +52,64 @@ export default () => {
   }
 
   return (
-    <div className='security-settings-wrapper'>
+    <div className="security-settings-wrapper">
       <div
-        className='security-settings'
+        className="security-settings"
         style={{
           filter: `${
-            showExportBackupPhraseModal || showExportBackupKeyfileModal
-              ? 'blur(8px)'
-              : 'none'
-          }`,
+            showExportBackupPhraseModal || showExportBackupKeyfileModal ? 'blur(8px)' : 'none'
+          }`
         }}
       >
-        <div className='header'>Security Settings</div>
-        <div className='content'>
-          <div className='backup-seedphrase'>
-            <div className='title'>Get my Backup (seed) Phrase</div>
-            <div className='description'>
-              Select a wallet to see its recovery phrase.
-            </div>
-            <div className='seedphrase'>
+        <div className="header">Security Settings</div>
+        <div className="content">
+          <div className="backup-seedphrase">
+            <div className="title">Get my Backup (seed) Phrase</div>
+            <div className="description">Select a wallet to see its recovery phrase.</div>
+            <div className="seedphrase">
               {accounts.map((account) => {
-                if (account.seedPhrase) return (
-                  <div
-                    key={account.id}
-                    className='account'
-                    onClick={() => onSeedPhraseClick(account)}
-                  >
-                    <div className='name-icon'>
-                      {account.type === TYPE.ARWEAVE && <div className='finnie-icon'><FinnieIcon /></div>}
-                      {account.type === TYPE.ETHEREUM && <div className='finnie-icon'><EthereumIcon /></div>}
-                      <div className='account-name'>{account.accountName}</div>
+                if (account.seedPhrase)
+                  return (
+                    <div
+                      key={account.id}
+                      className="account"
+                      onClick={() => onSeedPhraseClick(account)}
+                    >
+                      <div className="name-icon">
+                        {account.type === TYPE.ARWEAVE && (
+                          <div className="finnie-icon">
+                            <FinnieIcon />
+                          </div>
+                        )}
+                        {account.type === TYPE.ETHEREUM && (
+                          <div className="finnie-icon">
+                            <EthereumIcon />
+                          </div>
+                        )}
+                        <div className="account-name">{account.accountName}</div>
+                      </div>
+                      <div className="account-address">{getDisplayAddress(account.address)}</div>
                     </div>
-                    <div className='account-address'>
-                      {getDisplayAddress(account.address)}
-                    </div>
-                  </div>
-                )
+                  )
               })}
             </div>
           </div>
 
-          <div className='backup-keyfile'>
-            <div className='title'>Export my Private Key</div>
-            <div className='description'>
-              Select a wallet to download its private key.
-            </div>
-            <div className='keyfile'>
+          <div className="backup-keyfile">
+            <div className="title">Export my Private Key</div>
+            <div className="description">Select a wallet to download its private key.</div>
+            <div className="keyfile">
               {arAccounts.map((account) => (
                 <div
                   key={account.address}
-                  className='account'
+                  className="account"
                   onClick={() => onKeyFileClick(account)}
                 >
-                  <div className='name-icon'>
-                    <FinnieIcon className='finnie-icon' />
-                    <div className='account-name'>{account.accountName}</div>
+                  <div className="name-icon">
+                    <FinnieIcon className="finnie-icon" />
+                    <div className="account-name">{account.accountName}</div>
                   </div>
-                  <div className='account-address'>
-                    {getDisplayAddress(account.address)}
-                  </div>
+                  <div className="account-address">{getDisplayAddress(account.address)}</div>
                 </div>
               ))}
             </div>
@@ -129,17 +118,11 @@ export default () => {
       </div>
 
       {showExportBackupPhraseModal && (
-        <ExportBackupPhraseModal
-          account={selectedAccount}
-          closeModal={closeModal}
-        />
+        <ExportBackupPhraseModal account={selectedAccount} closeModal={closeModal} />
       )}
 
       {showExportBackupKeyfileModal && (
-        <ExportBackupKeyFileModal
-          account={selectedAccount}
-          closeModal={closeModal}
-        />
+        <ExportBackupKeyFileModal account={selectedAccount} closeModal={closeModal} />
       )}
     </div>
   )
