c08ac25053bd2c26f39970cfa5ae531b36df337f
thuan.ngo <thuan.ngo@eastagile.com>
2022-01-31 15:31:36 +0700

    [r] 239 - Format code notification row and activity row
    [#181086239] https://www.pivotaltracker.com/n/projects/2497930/stories/181086239

--- src/finnie-v2/components/ActivityCenterDropdown/NotificationRow/NotificationRow.js
+++ src/finnie-v2/components/ActivityCenterDropdown/NotificationRow/NotificationRow.js
@@ -8,14 +8,17 @@ import { setNotifications } from 'options/actions/notifications'
 
 const NotificationRow = ({ notification, newNotification = false }) => {
   const dispatch = useDispatch()
-  
+
   const dateFormat = (date) => {
     return moment(date).format('MMMM Do, YYYY')
   }
 
   const setRead = async () => {
     let allNotifications = await storage.generic.get.pushNotification()
-    allNotifications = allNotifications.map(n => {if (n.txId === notification.txId) n.new = false; return n})
+    allNotifications = allNotifications.map((n) => {
+      if (n.txId === notification.txId) n.new = false
+      return n
+    })
     dispatch(setNotifications(allNotifications))
     storage.generic.set.pushNotification(allNotifications)
 
@@ -25,7 +28,10 @@ const NotificationRow = ({ notification, newNotification = false }) => {
   }
 
   return (
-    <div onClick={setRead} className="flex flex-col px-4.25 pl-5 h-18.25 border-b-2 border-gray-underline cursor-pointer">
+    <div
+      onClick={setRead}
+      className="flex flex-col px-4.25 pl-5 h-18.25 border-b-2 border-gray-underline cursor-pointer"
+    >
       <div className="w-full flex flex-col relative pt-2">
         {newNotification && (
           <div className="absolute -left-4.25 top-3.25">
@@ -33,7 +39,9 @@ const NotificationRow = ({ notification, newNotification = false }) => {
           </div>
         )}
         <div className="text-xs text-blue-800 font-semibold leading-5">{notification.title}</div>
-        <div className="text-xs text-blue-800 leading-5 h-5.25 overflow-hidden">{notification.message}</div>
+        <div className="text-xs text-blue-800 leading-5 h-5.25 overflow-hidden">
+          {notification.message}
+        </div>
       </div>
       <div className="w-full flex justify-between">
         <div className="text-xs text-blue-800 font-semibold leading-5">{notification.account}</div>
