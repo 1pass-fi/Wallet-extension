e00d65a5b075bb52f4a8e4d79233db12fd3dc588
thuan.ngo <thuan.ngo@eastagile.com>
2022-02-02 15:59:01 +0700

    [c] 987 - ui polishing - Collection titles formatting
    https://app.clickup.com/t/1yuf1eu

--- src/finnie-v2/components/CollectionCard/CollectionCard.js
+++ src/finnie-v2/components/CollectionCard/CollectionCard.js
@@ -20,15 +20,15 @@ const CollectionCard = ({ collection }) => {
       <div className="rounded shadow absolute top-1.375 left-1.75 bg-blue-800 w-47.25 h-56.5" />
       <div className="rounded shadow absolute top-2.75 left-0 bg-blue-800 w-47.25 h-56.5" />
 
-      <div className="rounded absolute top-5 left-2 flex justify-center items-center w-43 h-40">
-        {nft ? (
-          <NFTMedia contentType={nft.contentType} source={nft.imageUrl} />
-        ) : (
-          <NFTMedia contentType="not-register-data" />
-        )}
-      </div>
-      <div className="text-base absolute w-47.25 left-0 bottom-7 flex justify-center">
-        <div className="tracking-finnieSpacing-wide text-center">
+      <div className="absolute top-5 left-0 w-47.25 flex flex-col items-center">
+        <div className="rounded flex justify-center items-center w-43 h-40">
+          {nft ? (
+            <NFTMedia contentType={nft.contentType} source={nft.imageUrl} />
+          ) : (
+            <NFTMedia contentType="not-register-data" />
+          )}
+        </div>
+        <div className="mt-1 text-base tracking-finnieSpacing-wide text-center">
           {formatLongString(collection.name, 20)}
         </div>
       </div>
