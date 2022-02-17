c31a5a90fc7e2754ac10fe8324704dde341a0651
Long Pham <long.pham@eastagile.com>
2022-02-17 14:59:02 +0700

    [b] - rwj - Update height of tags field
    https://app.clickup.com/t/202jrwj

--- src/options/pages/NFTDetail/NFTDetail.js
+++ src/options/pages/NFTDetail/NFTDetail.js
@@ -184,7 +184,7 @@ const NFTDetail = () => {
               <p className="w-full max-h-12 overflow-y-scroll overflow-x-none break-words text-sm leading-6 pr-4 whitespace-pre-line">
                 {nft.description}
               </p>
-              <div className="mt-3.25 flex flex-wrap gap-x-2.5 gap-y-1 h-11 overflow-y-scroll">
+              <div className="mt-3.25 flex flex-wrap gap-x-2.5 gap-y-1 max-h-11 overflow-y-scroll">
                 {nft.tags &&
                   nft.tags.map((tag) => (
                     <div
