0714a6933764f373168ad7bad9937a49ade2d717
Minh Vu <minh.vu@EAs-iMac.local>
2022-02-17 17:31:56 +0700

    [f] - 475 - ethereum api - Ethereum provider

---
+++ src/content_scripts/FinnieEthereumProvider.js
@@ -0,0 +1,20 @@
+const finnieEthereumProviderScript = () => {
+  class FinnieEthereumProvider extends EventEmitter {
+    constructor(connection) {
+      super()
+
+      this.connection = connection
+    }
+
+    async request(message) {
+      try {
+        const response = await this.connection.send(message)
+        console.log('RESPONSE', response)
+      } catch (err) {
+        console.error('ERROR --- ', err.message)
+      }
+    }
+  }
+}
+
+export default declareFinnieEthereumProvider
