782ab88931133e202931ddf64945a567d5bcea56
thuan.ngo <thuan.ngo@eastagile.com>
2022-02-21 15:19:39 +0700

    [f] 475 - Update home page tabs to use router
    [#181325475] https://www.pivotaltracker.com/story/show/181325475

--- src/popup/pages/Home/AccountInfo/AccountInfo.js
+++ src/popup/pages/Home/AccountInfo/AccountInfo.js
@@ -1,36 +1,43 @@
 import clsx from 'clsx'
-import capitalize from 'lodash/capitalize'
-import React, { useState } from 'react'
+import React from 'react'
+import { Switch, Route, NavLink, Redirect } from 'react-router-dom'
 
 import Tokens from './Tokens'
 import Assets from './Assets'
 
-const tabs = ['ASSETS', 'TOKENS', 'ACTIVITY']
+const tabs = [
+  { name: 'Assets', to: '/assets' },
+  { name: 'Tokens', to: '/tokens' },
+  { name: 'Activity', to: '/activity' }
+]
 
 const AccountInfo = () => {
-  const [currentTab, setCurrentTab] = useState('TOKENS')
-
   return (
     <div className="z-20 bg-trueGray-100 text-blue-600 text-base flex flex-col h-full">
       <div className="shadow-lg h-10.75 flex items-stretch">
         {tabs.map((tab, idx) => (
-          <div
+          <NavLink
             key={idx}
-            className={clsx(
-              'w-1/3 h-10.75 flex items-center justify-center cursor-pointer',
-              currentTab === tab && 'font-semibold bg-lightBlue'
-            )}
-            onClick={() => setCurrentTab(tab)}
+            to={tab.to}
+            className={clsx('w-1/3 h-10.75 flex items-center justify-center cursor-pointer')}
+            activeClassName="font-semibold bg-lightBlue"
           >
-            {capitalize(tab)}
-          </div>
+            {tab.name}
+          </NavLink>
         ))}
       </div>
-      <div>
-        {currentTab === 'TOKENS' && <Tokens />}
-        {currentTab === 'ASSETS' && <Assets />}
-        {currentTab === 'ACTIVITY' && <div>Activity</div>}
-      </div>
+      <Switch>
+        <Route exact path="/assets">
+          <Assets />
+        </Route>
+        <Route exact path="/activity">
+          <div>Activity</div>
+        </Route>
+        <Route exact path="/tokens">
+          <Tokens />
+        </Route>
+        <Redirect to="/tokens" />
+      </Switch>
     </div>
   )
 }
