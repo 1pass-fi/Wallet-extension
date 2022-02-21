98e75cf3a2eaff2b44f5af19776bc2a7b6ec030d
Minh Vu <minh.vu@EAs-iMac.local>
2022-02-21 21:58:20 +0700

    [f] - popup redesign - pause finnie dropdown UI

--- src/popup/components/PopupHeader/PopupHeader.js
+++ src/popup/components/PopupHeader/PopupHeader.js
@@ -7,9 +7,11 @@ import Account from './Account'
 import Avatar from 'img/popup/avatar-icon.svg'
 import OptionIcon from 'img/popup/option-icon.svg'
 import SettingIcon from 'img/popup/setting-icon.svg'
+import PauseIcon from 'img/popup/pause-icon.svg'
 
 const Header = () => {
   const [showAccountDropdown, setShowAccountDropdown] = useState(false)
+  const [showPauseFinnieDropdown, setShowPauseFinnieDropdown] = useState(false)
   const defaultAccount = useSelector((state) => state.defaultAccount)
 
   const goToSetting = () => {
@@ -45,9 +47,16 @@ const Header = () => {
       >
         <Avatar className="mt-1.25" />
       </div>
-      <div className="bg-blue-800 flex items-center justify-center" style={{ width: '30px' }}>
+      <div onClick={() => setShowPauseFinnieDropdown(prev => !prev)} onMouseEnter={() => setShowPauseFinnieDropdown(true)} className="bg-blue-800 flex items-center justify-center" style={{ width: '30px' }}>
         <OptionIcon />
       </div>
+      {showPauseFinnieDropdown && <div style={{width:'252px',height:'92px',right:'0px',top:'54px'}} className='text-base text-white absolute bg-'>
+        <div style={{height:'46px',paddingRight:'16px',borderBottom:'1px solid #8585BC'}} className='bg-blue-800 hover:bg-blue-400 cursor-pointer flex items-center justify-end'>
+          <div style={{marginRight:'6px'}}><PauseIcon /></div>
+          Pause Finnie on this site
+        </div>
+        <div style={{height:'46px',paddingRight:'16px'}} className='bg-blue-800 hover:bg-blue-400 cursor-pointer flex items-center justify-end'>Report an Issue</div>
+      </div>}
     </div>
   )
 }
