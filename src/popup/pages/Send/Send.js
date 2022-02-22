a122222426f322f336ae3ef3ac1554396c2eff18
Long Pham <long.pham@eastagile.com>
2022-02-22 16:35:17 +0700

    [f] 793 - Add Send Token form
    [#181324793] https://www.pivotaltracker.com/story/show/181324793

--- src/popup/pages/Send/Send.js
+++ src/popup/pages/Send/Send.js
@@ -1,13 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import ReactTooltip from 'react-tooltip'
+import clsx from 'clsx'
+import isEmpty from 'lodash/isEmpty'
 
+import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
+import TokenDropdown from 'popup/components/TokenDropdown'
+import SendTokenForm from './SendTokenForm'
+
+// constants
+import { ERROR_MESSAGE } from 'constants/koiConstants'
+import { TYPE } from 'constants/accountConstants'
+
+import { formatNumber } from 'options/utils'
+import { setError } from 'actions/error'
+import { isArweaveAddress, isEthereumAddress } from 'utils'
+import { popupAccount } from 'services/account'
+
+import FinnieIcon from 'img/v2/koii-logos/finnie-koii-logo-blue.svg'
+import EthereumIcon from 'img/v2/ethereum-logos/ethereum-logo.svg'
+import ArweaveIcon from 'img/v2/arweave-logos/arweave-logo.svg'
+import ArrowIconBlue from 'img/popup/down-arrow-icon-blue.svg'
+import BackBtn from 'img/popup/back-button.svg'
 import SendBackgroundLeft from 'img/popup/send-background-left.svg'
 import SendBackgroundRight from 'img/popup/send-background-right.svg'
-import BackBtn from 'img/popup/back-button.svg'
 
-const Send = () => {
+const Send = ({ setError }) => {
   const history = useHistory()
+
+  const defaultAccount = useSelector((state) => state.defaultAccount)
+
+  const [fontSize, setFontSize] = useState('3xl')
+  const [balance, setBalance] = useState(null)
+  const [koiBalance, setKoiBalance] = useState(null)
+
+  const [selectedToken, setSelectedToken] = useState()
+  const [tokenOptions, setTokenOptions] = useState([])
+  const [showTokenOptions, setShowTokenOptions] = useState(false)
+  const [amount, setAmount] = useState('')
+  const [recipient, setRecipient] = useState('')
+  const [selectedAccount, setSelectedAccount] = useState(null)
+  const [enoughGas, setEnoughGas] = useState(true)
+
+  useEffect(() => {
+    const getTokenOptions = async () => {
+      setSelectedToken(null)
+      const account = await popupAccount.getAccount({ address: selectedAccount.address })
+      let options
+      switch (selectedAccount.type) {
+        case TYPE.ARWEAVE:
+          options = [
+            { value: 'AR', label: 'AR' },
+            { value: 'KOII', label: 'KOII' }
+          ]
+          setTokenOptions(options)
+          setSelectedToken('KOII')
+          setBalance(await account.get.balance())
+          setKoiBalance(await account.get.koiBalance())
+          break
+        case TYPE.ETHEREUM:
+          options = [{ value: 'ETH', label: 'ETH' }]
+          setTokenOptions(options)
+          setSelectedToken('ETH')
+          setBalance(await account.get.balance())
+      }
+    }
+
+    if (selectedAccount) getTokenOptions()
+  }, [selectedAccount])
+
+  const onChangeToken = (selectedToken) => {
+    setSelectedToken(selectedToken)
+  }
+
+  const onChangeAmount = (e) => {
+    setAmount(e.target.value)
+    if (e.target.value.length <= 8) {
+      setFontSize('3xl')
+      return
+    }
+    if (e.target.value.length > 16) {
+      setFontSize('sm')
+      return
+    }
+    if (e.target.value.length > 12) {
+      setFontSize('lg')
+      return
+    }
+    if (e.target.value.length > 8) {
+      setFontSize('2xl')
+      return
+    }
+  }
+
+  const handleSendToken = () => {
+    // validations
+    if (!(recipient.trim().length > 0 && amount.trim().length > 0)) {
+      setError(ERROR_MESSAGE.EMPTY_FIELDS)
+      return
+    }
+    if (Number(amount) < 0) {
+      setError(ERROR_MESSAGE.INVALID_AMOUNT)
+      return
+    }
+    if (isEmpty(selectedAccount)) {
+      setError(ERROR_MESSAGE.SELECT_ACCOUNT)
+      return
+    }
+    if (isEmpty(selectedToken)) {
+      setError(ERROR_MESSAGE.SELECT_TOKEN)
+      return
+    }
+    if (Number(amount) === 0) {
+      setError(ERROR_MESSAGE.SEND_ZERO_KOI)
+      return
+    }
+
+    if (selectedAccount.type === TYPE.ARWEAVE && !isArweaveAddress(recipient)) {
+      setError('Invalid AR Address')
+      return
+    }
+
+    if (selectedAccount.type === TYPE.ETHEREUM && !isEthereumAddress(recipient)) {
+      setError('Invalid ETH Address')
+      return
+    }
+
+    setShowModal(true)
+  }
 
   return (
     <div className="w-full relative bg-white flex flex-col items-center pt-9.75">
@@ -17,8 +139,87 @@ const Send = () => {
         onClick={() => history.goBack()}
         className="w-8.75 h-8.75 z-20 absolute top-3.25 left-3.75 cursor-pointer bg-white bg-opacity-70 rounded-full"
       />
+
+      <div className="font-semibold text-xs leading-4 tracking-finnieSpacing-wide text-blue-800">
+        AMOUNT
+      </div>
+      <div
+        className="z-10 bg-trueGray-100 border-b-2 border-blue-800 flex"
+        style={{ width: '241px', height: '45px' }}
+        data-tip={isEmpty(selectedAccount) ? 'Please choose Sender Account first!' : ''}
+      >
+        <input
+          className={clsx(
+            isEmpty(selectedAccount) && 'cursor-not-allowed',
+            `text-${fontSize}`,
+            'text-blue-800 outline-none text-right tracking-finnieSpacing-tightest leading-12 border-b-2 border-blue-800 bg-trueGray-100 pr-1.5 pl-1'
+          )}
+          style={{ width: '173px', height: '45px' }}
+          type="number"
+          onChange={onChangeAmount}
+          value={amount}
+          disabled={isEmpty(selectedAccount)}
+        ></input>
+        <div
+          className={clsx(
+            isEmpty(selectedAccount) ? 'cursor-not-allowed' : 'cursor-pointer',
+            'relative flex items-center justify-evenly'
+          )}
+          onClick={() => {
+            !isEmpty(selectedAccount) && setShowTokenOptions((prev) => !prev)
+          }}
+          style={{ width: '68px', height: '45px' }}
+        >
+          {selectedToken === 'AR' && <ArweaveIcon style={{ width: '35px', height: '35px' }} />}
+          {selectedToken === 'ETH' && <EthereumIcon style={{ width: '33px', height: '33px' }} />}
+          {selectedToken === 'KOII' && <FinnieIcon style={{ width: '34px', height: '34px' }} />}
+          <ArrowIconBlue style={{ transform: !showTokenOptions ? 'none' : 'rotateX(180deg)' }} />
+          {showTokenOptions && (
+            <TokenDropdown
+              tokenOptions={tokenOptions}
+              selectedToken={selectedToken}
+              onChangeToken={onChangeToken}
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="text-success-700 text-base font-normal tracking-finnieSpacing-tight leading-8 select-none">
+        {selectedToken
+          ? `${
+            selectedToken === 'KOII'
+              ? formatNumber(defaultAccount.koiBalance, 2)
+              : formatNumber(defaultAccount.balance, 6)
+          } ${selectedToken} Available`
+          : ''}
+      </div>
+
+      <div
+        className="absolute bottom-0 w-full flex flex-col bg-trueGray-100"
+        style={{ height: '320px' }}
+      >
+        <SendTokenForm
+          handleSendToken={handleSendToken}
+          recipient={recipient}
+          setRecipient={setRecipient}
+          selectedAccount={selectedAccount}
+          setSelectedAccount={setSelectedAccount}
+          enoughGas={enoughGas}
+        />
+      </div>
+      <ReactTooltip place="top" effect="float" />
     </div>
   )
 }
 
-export default Send
+const mapStateToProps = (state) => ({
+  price: state.price,
+  currency: state.currency,
+  accounts: state.accounts
+})
+
+const mapDispatchToProps = {
+  setError
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Send)
---
+++ src/popup/pages/Send/SendTokenForm.js
@@ -0,0 +1,185 @@
+// modules
+import React, { useState, useEffect, useRef } from 'react'
+import ReactTooltip from 'react-tooltip'
+import find from 'lodash/find'
+import isEmpty from 'lodash/isEmpty'
+
+// components
+import Select from 'shared/select'
+
+// assets
+import DownArrowIcon from 'img/down-arrow-icon.svg'
+
+// services
+import { popupAccount } from 'services/account'
+
+// utils
+import { getAddressesFromAddressBook } from 'utils'
+
+// svgs
+import NoticeIcon from 'img/v2/notice-icon.svg'
+
+const SendTokenForm = ({
+  handleSendToken,
+  recipient,
+  setRecipient,
+  selectedAccount,
+  setSelectedAccount,
+  enoughGas
+}) => {
+  const [accountOptions, setAccountOptions] = useState([])
+  const [addressOptions, setAddressOptions] = useState([])
+  const [isShowDropdown, setIsShowDropdown] = useState(false)
+
+  const addressInputRef = useRef()
+
+  useEffect(() => {
+    const getAccountOptions = async () => {
+      const arAccounts = await popupAccount.getAllMetadata()
+      const options = arAccounts.map((account, index) => ({
+        id: index,
+        value: account.address,
+        label: account.accountName,
+        address: account.address,
+        type: account.type
+      }))
+
+      setAccountOptions(options)
+    }
+
+    getAccountOptions()
+  }, [])
+
+  useEffect(() => {
+    const getAddressList = async () => {
+      const options = await getAddressesFromAddressBook()
+      setAddressOptions(options)
+    }
+
+    getAddressList()
+  }, [])
+
+  const onChangeAccount = (selected) => {
+    const account = find(accountOptions, (v) => v.value == selected)
+    setSelectedAccount(account)
+  }
+
+  const onChangeRecipientAddress = (e) => {
+    setRecipient(e.target.value)
+  }
+
+  const onAddressDropdownChange = (account) => {
+    if (isEmpty(account)) {
+      setRecipient('')
+      addressInputRef.current.focus()
+    } else {
+      setRecipient(account.address)
+    }
+
+    setIsShowDropdown(false)
+  }
+
+  const AddressDropdown = ({ accounts = [], onChange, type }) => {
+    return (
+      <div className="accounts">
+        <div className="different-address" onClick={() => onChange({})}>
+          <div className="name">Enter Address Manually</div>
+        </div>
+        {accounts.map((account) => {
+          if (account.type === type) {
+            return (
+              <div key={account.id} className="account" onClick={() => onChange(account)}>
+                <div className="logo">
+                  {account.type === TYPE.ARWEAVE && <ArweaveLogo />}
+                  {account.type === TYPE.ETHEREUM && <EthereumLogo />}
+                </div>
+                <div className="info">
+                  <div className="name">{account.accountName}</div>
+                  <div className="address">{getDisplayAddress(account.address)}</div>
+                </div>
+              </div>
+            )
+          }
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <div className="pt-7.5 flex flex-col items-center">
+      {/* SELECT ACCOUNT */}
+      <Select
+        options={accountOptions}
+        placeholder="Select Account"
+        onChange={onChangeAccount}
+        label="FROM"
+        isAccountAddress={true}
+      />
+      {/* RECIPIENT INPUT */}
+      <div className="pt-7 recipient">
+        <div className="label">TO</div>
+        <div className="recipient-input">
+          <input
+            ref={(ip) => (addressInputRef.current = ip)}
+            value={recipient}
+            onChange={onChangeRecipientAddress}
+            className="recipient-input-field"
+            placeholder="Recipient’s Wallet Address"
+          />
+          <div
+            className="address-dropdown"
+            data-tip={isEmpty(selectedAccount) ? 'Please choose Sender Account first!' : ''}
+            data-for="arrow-button"
+          >
+            <button
+              className="arrow-button"
+              disabled={isEmpty(selectedAccount)}
+              onClick={(e) => {
+                e.preventDefault()
+                setIsShowDropdown((isShowDropdown) => !isShowDropdown)
+              }}
+            >
+              <div
+                className="arrow-icon"
+                style={{ transform: !isShowDropdown ? 'none' : 'rotateX(180deg)' }}
+              >
+                <DownArrowIcon />
+              </div>
+            </button>
+            <ReactTooltip place="top" effect="float" />
+            <ReactTooltip id="arrow-button" place="left" effect="float" />
+          </div>
+          {isShowDropdown && !isEmpty(selectedAccount) && (
+            <AddressDropdown
+              accounts={addressOptions}
+              onChange={onAddressDropdownChange}
+              type={selectedAccount.type}
+            />
+          )}
+        </div>
+      </div>
+
+      {!enoughGas && (
+        <div
+          className="mt-4.5 flex items-center bg-warning font-normal text-sm leading-6 text-center text-indigo rounded-sm"
+          type="send-tokens"
+          style={{ width: '219px', height: '24px' }}
+        >
+          <NoticeIcon className="mx-1.75" style={{ width: '17px', height: '17px' }} />
+          Not Enough Funds or Gas
+        </div>
+      )}
+
+      <button
+        className="absolute bottom-15 bg-lightBlue font-normal text-sm text-center text-blue-800 leading-4 border border-lightBlue rounded-sm shadow"
+        type="send-tokens"
+        style={{ width: '200px', height: '38px' }}
+        onClick={() => handleSendToken()}
+      >
+        Send Tokens
+      </button>
+    </div>
+  )
+}
+
+export default SendTokenForm
