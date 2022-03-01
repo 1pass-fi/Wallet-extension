755501b3f1436a24fd3e31b18f02f3a086eaa989
thuan.ngo <thuan.ngo@eastagile.com>
2022-03-01 16:29:19 +0700

    [f] f08 - Copy wallet address
    https://app.clickup.com/t/2088f08

--- src/options/finnie-v2/components/AccountManagement/AccountManagement.js
+++ src/options/finnie-v2/components/AccountManagement/AccountManagement.js
@@ -1,12 +1,14 @@
 import clsx from 'clsx'
 import React, { useMemo, useState, useContext } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 import DropDown from 'finnie-v2/components/DropDown'
 import formatLongString from 'finnie-v2/utils/formatLongString'
 
 import ArLogo from 'img/v2/arweave-logos/arweave-logo.svg'
 import EthLogo from 'img/v2/ethereum-logos/ethereum-logo.svg'
+import CopyIcon from 'img/v2/copy-icon.svg'
 
 import storage from 'services/storage'
 import { popupBackgroundRequest as backgroundRequest } from 'services/request/popup'
@@ -17,6 +19,30 @@ import { DidContext } from 'options/context'
 import CheckBox from 'finnie-v2/components/CheckBox'
 
 const tabs = ['AR', 'ETH']
+
+const Address = ({ address }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const onCopy = () => {
+    setIsCopied(true)
+
+    setTimeout(() => setIsCopied(false), 2000)
+  }
+
+  return (
+    <>
+      {formatLongString(address, 22)}
+      <div className="relative inline ">
+        <CopyToClipboard text={address} onCopy={onCopy}>
+          <CopyIcon className="inline cursor-pointer w-3.25 ml-2" />
+        </CopyToClipboard>
+        {isCopied && (
+          <span className="text-11px absolute top-0 -right-14 text-blue-800">Copied!</span>
+        )}
+      </div>
+    </>
+  )
+}
 
 const AccountManagement = ({ accounts }) => {
   const { setNotification, setError } = useContext(GalleryContext)
@@ -106,7 +132,9 @@ const AccountManagement = ({ accounts }) => {
                 )}
                 {formatLongString(account.accountName, 12)}
               </td>
-              <td>{formatLongString(account.address, 22)}</td>
+              <td>
+                <Address address={account.address} key={account.address} />
+              </td>
               {/* Still in use */}
               {/* <td className="w-50 pr-10">
                 <DropDown size="sm" variant="light" options={options} value="mainnet" />
