baabacce4146a514f1362d5abc15656c848472b3
thuan.ngo <thuan.ngo@eastagile.com>
2022-03-17 23:06:57 +0700

    [f] 1ft -[WIP] get ETH key
    [2d3x1ft] https://app.clickup.com/t/2d3x1ft

--- src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethSendTransaction.js
+++ src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethSendTransaction.js
@@ -1,9 +1,34 @@
-export default (payload, tab, next) => {
+import Web3 from 'web3'
+
+import { backgroundAccount } from 'services/account'
+import storage from 'services/storage'
+
+export default async (payload, tab, next) => {
   try {
     console.log('=== TEST SEND ETH TRANSACTION ===')
     // TODO: ThuanN
 
-    next({data: 'mocked_sendTransaction'})
+    const rawTx = {
+      from: '0xb076413401172CBB73C082107514De3376E4FF6c',
+      to: '0x0c54FcCd2e384b4BB6f2E405Bf5Cbc15a017AaFb',
+      value: '0x0',
+      gas: 0,
+      type: '0x0'
+    }
+
+    const account = await backgroundAccount.getAccount({ address: rawTx.from })
+
+    const provider = await storage.setting.get.ethereumProvider()
+
+    const web3 = new Web3(provider)
+
+    const estimateGas = await web3.eth.estimateGas(rawTx)
+    rawTx.gas = estimateGas
+
+    const signTx = await web3.eth.accounts.signTransaction(rawTx, account.mockedGetKey())
+    const receipt = await web3.eth.sendSignedTransaction(signTx.rawTransaction)
+
+    next({ data: receipt })
   } catch (err) {
     next({ error: err.message })
   }
--- src/services/account/Account/index.js
+++ src/services/account/Account/index.js
@@ -53,7 +53,7 @@ export class EthereumAccount extends BackgroundAccount {
 
   mockedGetKey() {
     // TODO: ThuanN
-    return '0x939eeb8b935762306c76ce02ce7947b18ff4dcdac2aba553c2660b0cd673ff6f'
+    return '939eeb8b935762306c76ce02ce7947b18ff4dcdac2aba553c2660b0cd673ff6f'
   }
 
   static utils = new EthereumStatic()
