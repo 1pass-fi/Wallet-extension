4330231b282d9e84979a7d7a820eb4fb58ce9bfb
Thuan Ngo <thuan.ngo@eastagile.com>
2022-03-24 15:18:36 +0700

    [f] x26 - Decrypt message
    https://app.clickup.com/t/2g54x26

--- src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethDecrypt.js
+++ src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethDecrypt.js
@@ -1,7 +1,33 @@
-export default (payload, tab, next) => {
+import { get } from 'lodash'
+
+import { stripHexPrefix } from 'ethereumjs-util'
+import { decrypt } from '@metamask/eth-sig-util'
+
+import { backgroundAccount } from 'services/account'
+import storage from 'services/storage'
+
+export default async (payload, tab, next) => {
   try {
-    const mockedDecryptedData = 'DecryptedData'
-    next({ data: { responseData: mockedDecryptedData, id: payload.data.id } })
+    const { hadPermission } = tab
+    if (!hadPermission) {
+      return next({ error: { code: 4100, data: 'No permissions' } })
+    }
+
+    const params = get(payload, 'data.params')
+    const encryptedDataHex = params[0]
+
+    const stripped = stripHexPrefix(encryptedDataHex)
+    const buff = Buffer.from(stripped, 'hex')
+    const encryptedData = JSON.parse(buff.toString('utf8'))
+
+    const defaultEthereumAddress = await storage.setting.get.activatedEthereumAccountAddress()
+    const credential = await backgroundAccount.getCredentialByAddress(defaultEthereumAddress)
+
+    const privateKey = stripHexPrefix(credential.key)
+
+    const decryptedMessage = decrypt({ privateKey, encryptedData })
+
+    next({ data: decryptedMessage })
   } catch (err) {
     next({ error: err.message })
   }
