e9240b8bd3b8d63650e4d9d4a8821b6855dbd56d
Long Pham <long.pham@eastagile.com>
2022-04-02 15:34:49 +0700

    [f] jnz - Implement search tokens
    https://app.clickup.com/t/2j3wjnz

--- src/popup/pages/ImportToken/ImportToken.js
+++ src/popup/pages/ImportToken/ImportToken.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Switch, Route, NavLink, Redirect } from 'react-router-dom'
+import isEmpty from 'lodash/isEmpty'
 
 import BackBtn from 'img/popup/back-button.svg'
+import FinnieIcon from 'img/popup/finnie-icon-blue.svg'
 
 import Search from './Search'
 import CustomToken from './CustomToken'
+import clsx from 'clsx'
 
 const tabs = [
-  { name: 'Search', to: '/import-token/search' },
-  { name: 'Custom Token', to: '/import-token/custom-token' }
+  { name: 'Search', to: 'SEARCH_TOKEN' },
+  { name: 'Custom Token', to: 'CUSTOM_TOKEN' }
 ]
 
 export const ImportToken = () => {
   const history = useHistory()
+
+  const [currentTab, setCurrentTab] = useState('SEARCH_TOKEN')
+  const [tokenImport, setTokenImport] = useState({})
 
   return (
     <div>
@@ -22,36 +27,51 @@ export const ImportToken = () => {
         style={{ width: '426px', height: '48px' }}
       >
         <BackBtn
-          onClick={() => {
-            console.log('go back')
-            history.goBack()
-          }}
+          onClick={() => history.goBack()}
           className="w-8.75 h-8.75 ml-3.75 cursor-pointer bg-white bg-opacity-70 rounded-full"
         />
         <div className="ml-6 font-semibold text-lg text-blue-800">Import a Token</div>
       </div>
 
-      <div className="flex sticky font-normal text-base leading-5 text-blue-850">
-        {tabs.map((tab, idx) => (
-          <NavLink
-            key={idx}
-            to={tab.to}
-            className="w-1/3 h-10.75 flex items-center justify-center cursor-pointer"
-            activeClassName="text-blue-800 underline"
-          >
-            {tab.name}
-          </NavLink>
-        ))}
-      </div>
-      <Switch>
-        <Route exact path="/import-token/search">
-          <Search />
-        </Route>
-        <Route exact path="/import-token/custom-token">
-          <CustomToken />
-        </Route>
-        <Redirect to="/import-token/search" />
-      </Switch>
+      {isEmpty(tokenImport) ? (
+        <>
+          <div className="flex sticky font-normal text-base leading-5 text-blue-850">
+            {tabs.map((tab, idx) => (
+              <div
+                key={idx}
+                className={clsx(
+                  'ml-18 h-10.75 flex items-center justify-center cursor-pointer',
+                  currentTab === tab.to && 'text-blue-800 underline'
+                )}
+                onClick={() => setCurrentTab(tab.to)}
+              >
+                {tab.name}
+              </div>
+            ))}
+          </div>
+          {currentTab === 'SEARCH_TOKEN' && <Search setTokenImport={setTokenImport} />}
+          {currentTab === 'CUSTOM_TOKEN' && <CustomToken />}
+        </>
+      ) : (
+        <div className="mt-6.25 px-6 text-blue-800">
+          <div className="font-normal text-base tracking-finnieSpacing-tight">
+            To which wallet(s) do you want to import these tokens?
+          </div>
+          <div className="mt-6.25 text-xs font-normal tracking-finnieSpacing-wide">Token</div>
+          <div className="flex items-center mt-3.75">
+            <FinnieIcon style={{ width: '36px', height: '36px' }} />
+            <div className="ml-3 font-normal text-base tracking-finnieSpacing-tight">
+              {tokenImport.name + ' (' + tokenImport.symbol + ')'}
+            </div>
+          </div>
+
+          <div className="mt-8 grid grid-cols-3">
+            <div className="font-normal text-xs tracking-finnieSpacing-wide">Account</div>
+            <div className="font-normal text-xs tracking-finnieSpacing-wide">Balance</div>
+            <div className="font-normal text-xs tracking-finnieSpacing-wide">Add Token</div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/popup/pages/ImportToken/Search.js
+++ src/popup/pages/ImportToken/Search.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import isEmpty from 'lodash/isEmpty'
 
-const Search = () => {
-  return <div>Search</div>
+import SearchIcon from 'img/popup/search-icon.svg'
+import FinnieIcon from 'img/popup/finnie-icon-blue.svg'
+
+import contracts from 'utils/contract-map.json'
+
+const Search = ({ setTokenImport }) => {
+  const [searchToken, setSearchToken] = useState('')
+  const [tokenList, setTokenList] = useState([])
+
+  const onChangeSearchToken = (e) => {
+    if (e.keyCode === 13) {
+      onSearchToken()
+    }
+  }
+
+  const onSearchToken = async () => {
+    console.log('onSearchToken', searchToken)
+    if (isEmpty(searchToken)) return
+    const filterTokenList = Object.keys(contracts).reduce((result, key) => {
+      if (
+        key === searchToken ||
+        contracts[key].name?.includes(searchToken) ||
+        contracts[key].symbol?.includes(searchToken)
+      ) {
+        // TODO LongP
+        const token = {
+          contract: key,
+          name: contracts[key].name,
+          symbol: contracts[key].symbol
+        }
+        result.push(token)
+      }
+      return result
+    }, [])
+
+    console.log('onSearchToken', filterTokenList)
+    setTokenList(filterTokenList)
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-5.5">
+      <div
+        className="bg-trueGray-100 rounded border border-blue-800 flex justify-between items-center"
+        style={{ width: '352px', height: '32px' }}
+      >
+        <input
+          className="bg-trueGray-100 text-blue-800 placeholder-opacity-80 outline-none font-normal text-sm leading-6 pl-2"
+          placeholder="Seach for a token"
+          style={{ width: '316px' }}
+          onChange={(e) => setSearchToken(e.target.value)}
+          onKeyUp={(e) => onChangeSearchToken(e)}
+          value={searchToken}
+        ></input>
+        <SearchIcon className="mr-2 cursor-pointer" onClick={() => onSearchToken()} />
+      </div>
+
+      <div
+        className="mt-6.25 w-full flex flex-col pl-12.5 pr-1.5 mr-1.25 overflow-y-scroll"
+        style={{ height: '300px' }}
+      >
+        {tokenList.map((token, idx) => (
+          <div
+            key={idx}
+            className="flex w-full items-center mb-6 cursor-pointer"
+            onClick={() => setTokenImport(token)}
+          >
+            <FinnieIcon style={{ width: '36px', height: '36px' }} />
+            <div className="ml-3 font-normal text-base tracking-finnieSpacing-tight text-blue-800">
+              {token.name + ' (' + token.symbol + ')'}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Search
