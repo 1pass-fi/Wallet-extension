de562488353138d859d67c426971146b9b89e23c
Thuan Ngo <thuan.ngo@eastagile.com>
2022-04-04 17:51:59 +0700

    [f] - Get token data utils
    TODO: consider make it into a hook

---
+++ src/utils/getTokenData.js
@@ -0,0 +1,271 @@
+import axios from 'axios'
+import React from 'react'
+import get from 'lodash/get'
+import contractMap from '@metamask/contract-metadata'
+import Web3 from 'web3'
+
+import storage from 'services/storage'
+
+const ERC20_ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        name: '',
+        type: 'string'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_spender',
+        type: 'address'
+      },
+      {
+        name: '_value',
+        type: 'uint256'
+      }
+    ],
+    name: 'approve',
+    outputs: [
+      {
+        name: '',
+        type: 'bool'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_from',
+        type: 'address'
+      },
+      {
+        name: '_to',
+        type: 'address'
+      },
+      {
+        name: '_value',
+        type: 'uint256'
+      }
+    ],
+    name: 'transferFrom',
+    outputs: [
+      {
+        name: '',
+        type: 'bool'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        name: '',
+        type: 'uint8'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address'
+      }
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        name: 'balance',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        name: '',
+        type: 'string'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_to',
+        type: 'address'
+      },
+      {
+        name: '_value',
+        type: 'uint256'
+      }
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        name: '',
+        type: 'bool'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address'
+      },
+      {
+        name: '_spender',
+        type: 'address'
+      }
+    ],
+    name: 'allowance',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    payable: true,
+    stateMutability: 'payable',
+    type: 'fallback'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'owner',
+        type: 'address'
+      },
+      {
+        indexed: true,
+        name: 'spender',
+        type: 'address'
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256'
+      }
+    ],
+    name: 'Approval',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'from',
+        type: 'address'
+      },
+      {
+        indexed: true,
+        name: 'to',
+        type: 'address'
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256'
+      }
+    ],
+    name: 'Transfer',
+    type: 'event'
+  }
+]
+
+const getIconPath = (contractAddress) => {
+  const metadata = contractMap[contractAddress]
+  if (metadata?.logo) {
+    const fileName = metadata.logo
+    const path = `img/erc20/${fileName}`
+    return path
+  }
+}
+
+const getTokenData = async (contractAddresss, userAddress) => {
+  const icon = getIconPath(contractAddresss)
+
+  const provider = await storage.setting.get.ethereumProvider()
+  const web3 = new Web3(provider)
+  const tokenContract = new web3.eth.Contract(ERC20_ABI, contractAddress)
+
+  const name = await tokenContract.methods.name().call()
+  const decimal = await tokenContract.methods.decimals().call()
+  const symbol = await tokenContract.methods.symbol().call()
+  const balance = await tokenContract.methods.balanceOf(userAddress).call()
+
+  const selectedCurrency = (await storage.setting.get.selectedCurrency()) || 'USD'
+
+  const { data } = await axios.get(
+    `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${contractAddresss}&vs_currencies=${selectedCurrency}`
+  )
+
+  const price = get(data, [contractAddresss, selectedCurrency.toLowerCase()])
+
+  return {
+    icon,
+    balance,
+    price,
+    name,
+    symbol,
+    decimal
+  }
+}
+
+export default getTokenData
