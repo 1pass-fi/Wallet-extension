c2429e06ec12ea937705b0f8562cad26b502ee71
Thuan Ngo <thuan.ngo@eastagile.com>
2022-04-07 22:34:05 +0700

    [f] e6u - Infinite scroll for gallery page
    https://app.clickup.com/t/25zee6u

--- src/options/pages/Gallery/Gallery.js
+++ src/options/pages/Gallery/Gallery.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import NFTCard from 'finnie-v2/components/NFTCard'
 
+import './index.css'
+
 const Gallery = () => {
   const filteredNfts = useSelector((state) => state.assets.filteredNfts)
+  const [displayingNfts, setDisplayingNfts] = useState([])
+  const [dislayLength, setDisplayLength] = useState(filteredNfts.length)
+
+  useEffect(() => {
+    if (dislayLength === filteredNfts.length) {
+      return
+    } else {
+      setDisplayLength(filteredNfts.length)
+      setDisplayingNfts(filteredNfts.slice(0, 16))
+    }
+  }, [filteredNfts])
+
+  const handleScroll = (e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight
+    if (bottom) {
+      setDisplayingNfts([
+        ...displayingNfts,
+        ...filteredNfts.slice(displayingNfts.length, displayingNfts.length + 16)
+      ])
+    }
+  }
 
   return (
-    <div className="w-full flex justify-center items-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-5 place-items-center">
-        {filteredNfts.map((nft) => (
+    <div
+      id="gallery"
+      className="w-full flex justify-center items-center scroll"
+      onScroll={handleScroll}
+      style={{ height: '85vh', overflow: 'auto' }}
+    >
+      <div className="grid h-full grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-5 place-items-center">
+        {displayingNfts.map((nft) => (
           <NFTCard nft={nft} key={nft.txId} />
         ))}
       </div>
