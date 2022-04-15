f643f231a1ed836f8b456e5d5d8578f89dab4109
Long Pham <long.pham@eastagile.com>
2022-04-15 14:13:24 +0700

    [f] uqp - Move load assets to useNfts hook
    https://app.clickup.com/t/2juwuqd

--- src/options/provider/index.js
+++ src/options/provider/index.js
@@ -51,6 +51,7 @@ import { setDefaultAccount } from 'options/actions/defaultAccount'
 import { setCollections } from 'options/actions/collections'
 import { setAssets, setCollectionNfts } from 'options/actions/assets'
 import { addNotification, setNotifications } from 'options/actions/notifications'
+import { useNfts } from './hooks/useNfts'
 
 export default ({ children }) => {
   const { pathname } = useLocation()
@@ -105,6 +106,7 @@ export default ({ children }) => {
   const [modalStates, setModalStates] = useModal()
   const [settingStates, setSettingStates] = useSetting({ walletLoaded })
   useAddHandler({ setError, setNotification, setModalStates, setIsLoading })
+  useNfts({ setCollections, setIsLoading, walletLoaded, newAddress })
 
   /* 
     GET STATE FROM STORE
@@ -298,68 +300,6 @@ export default ({ children }) => {
     setFile(!isEmpty(acceptedFiles) ? acceptedFiles[0] : {})
     // if (!isEmpty(acceptedFiles)) history.push('/create')
   }, [acceptedFiles])
-
-  useEffect(() => {
-    // load nfts and collection from store, set to state
-    const loadAssetsFromStorage = async () => {
-      setIsLoading((prev) => ++prev)
-      let allCollections = await popupAccount.getAllCollections()
-      let allCollectionNfts = await popupAccount.getAllCollectionNfts()
-      dispatch(setCollections({ collections: allCollections, filteredCollections: allCollections }))
-      dispatch(setCollectionNfts({ collectionNfts: allCollectionNfts }))
-
-      console.log('all Collections', allCollections)
-      console.log('loading all contents')
-      let allAssets = await popupAccount.getAllAssets()
-      let validAssets = allAssets.filter((asset) => asset.name !== '...')
-
-      validAssets = classifyAssets(validAssets, allCollections)
-      console.log('valid assets', validAssets.length)
-      validAssets = validAssets.filter((nft) => !get(nft, 'name')?.includes('DID Profile Page'))
-
-      dispatch(setAssets({ nfts: validAssets, filteredNfts: validAssets }))
-
-      setIsLoading((prev) => --prev)
-    }
-
-    const fetchAssets = async () => {
-      let allCollections = await popupAccount.getAllCollections()
-      let allCollectionNfts = await popupAccount.getAllCollectionNfts()
-      let allAssets, validAssets
-
-      const loadCollection = async () => {
-        console.log('LOADING COLLECTION')
-        await backgroundRequest.gallery.loadCollections()
-        allCollections = await popupAccount.getAllCollections()
-        allCollectionNfts = await popupAccount.getAllCollectionNfts()
-        dispatch(
-          setCollections({ collections: allCollections, filteredCollections: allCollections })
-        )
-        dispatch(setCollectionNfts({ collectionNfts: allCollectionNfts }))
-      }
-
-      const loadNfts = async () => {
-        await backgroundRequest.assets.loadAllContent()
-        allAssets = await popupAccount.getAllAssets()
-        validAssets = allAssets.filter((asset) => asset.name !== '...')
-        validAssets = classifyAssets(validAssets, allCollections)
-        validAssets = validAssets.filter((nft) => !get(nft, 'name')?.includes('DID Profile Page'))
-        dispatch(setAssets({ nfts: validAssets, filteredNfts: validAssets }))
-      }
-
-      setIsLoading((prev) => ++prev)
-      await Promise.all([loadCollection, loadNfts].map((f) => f()))
-      validAssets = classifyAssets(validAssets, allCollections)
-      if (isEmpty(validAssets) && pathname === '/') {
-      } else {
-        dispatch(setAssets({ nfts: validAssets, filteredNfts: validAssets }))
-      }
-      setIsLoading((prev) => --prev)
-    }
-
-    loadAssetsFromStorage()
-    fetchAssets()
-  }, [walletLoaded, newAddress])
 
   return (
     <GalleryContext.Provider
