2c345d02479c1a62d09e555b24321caf55149d2f
longphamea <long.pham@eastagile.com>
2022-06-07 13:45:14 +0700

    [f] wjj - Create password screens
    https://app.clickup.com/t/2fmawjj

---
+++ src/options/finnie-v2/pages/Onboarding/NavBar/NavBar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import KoiIcon from 'img/v2/onboarding/finnie-koii-logo.svg'
+import BackIcon from 'img/v2/back-icon.svg'
+import LockIcon from 'img/v2/onboarding/lock-icon.svg'
+import KeyIcon from 'img/v2/onboarding/key-icon.svg'
+import EditIcon from 'img/v2/onboarding/edit-icon.svg'
+import SeedphraseIcon from 'img/v2/onboarding/seedphrase-icon.svg'
+import LockSelectedIcon from 'img/v2/onboarding/lock-selected-icon.svg'
+import KeySelectedIcon from 'img/v2/onboarding/key-selected-icon.svg'
+import EditSelectedIcon from 'img/v2/onboarding/edit-selected-icon.svg'
+import SeedphraseSelectedIcon from 'img/v2/onboarding/seedphrase-selected-icon.svg'
+
+import { onboardingSteps } from '../Welcome'
+
+const NavBar = ({ step, setStep }) => {
+  return (
+    <div className="w-1/3 h-full bg-blue-800 shadow-lg flex flex-col items-center overflow-hidden">
+      {step > 1 && (
+        <BackIcon className="absolute top-5.5 left-6" style={{ width: '45px', height: '45px' }} />
+      )}
+      <div className="flex flex-col items-center">
+        <KoiIcon style={{ width: '156px', height: '156px' }} />
+        {onboardingSteps[step] === 'CREATE_PASSWORD' && (
+          <div
+            className="font-normal tracking-finnieSpacing-tightest text-white -pt-4"
+            style={{ fontSize: '40px', lineHeight: '48px' }}
+          >
+            Welcome to the Finnie Wallet
+            <div className="mt-6.25 mb-8 font-normal text-lg tracking-finnieSpacing-tight  text-success">
+              Get set up in just 4 quick & easy steps.
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="w-full pl-6 flex flex-col items-start gap-6 pt-8 font-normal text-base leading-8 text-white">
+        <div className="flex items-center">
+          <LockIcon className="mr-5" />
+          Secure Finnie with a password.
+        </div>
+        <div className="flex items-center">
+          <KeyIcon className="mr-5" />
+          Create or import a key.
+        </div>
+        <div className="flex items-center">
+          <EditIcon className="mr-5" />
+          Write down your recovery phrase.
+        </div>
+        <div className="flex items-center">
+          <SeedphraseIcon className="mr-5" />
+          Confirm your recovery phrase.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
--- src/options/finnie-v2/pages/Onboarding/Welcome.js
+++ src/options/finnie-v2/pages/Onboarding/Welcome.js
@@ -1,77 +1,30 @@
-import React from 'react'
-import clsx from 'clsx'
+import React, { useEffect, useState } from 'react'
 
-import WelcomeBackgroundTop from 'img/v2/popup/welcome-background-top.svg'
-import WelcomeBackgroundBottom from 'img/v2/popup/welcome-background-bottom.svg'
-import CreateNewIcon from 'img/v2/popup/create-new-icon.svg'
-import ImportSeedphraseIcon from 'img/v2/popup/import-seedphrase-icon.svg'
-import ImportKeyIcon from 'img/v2/popup/import-key-icon.svg'
-import KoiIcon from 'img/finnie-koi-logo-white.svg'
+import NavBar from './NavBar'
+import Content from './Content'
+
+export const onboardingSteps = [
+  'CREATE_PASSWORD',
+  'CREATE_OR_IMPORT',
+  'GET_A_KEY ',
+  'PREPARE_SAVE_PHRASE',
+  'HIDDEN_PHRASE',
+  'SHOW_PHRASE',
+  'INPUT_PHRASE',
+  'REVEAL_PHRASE'
+]
 
 const Welcome = () => {
+  const [step, setStep] = useState(0)
+
+  useEffect(() => {
+    console.log('step', step)
+  }, [step])
+
   return (
     <div className="w-screen h-screen flex text-center">
-      <div className="w-2/5 h-full bg-blue-800 shadow-lg grid grid-cols-1 grid-rows-7 justify-items-center">
-        <KoiIcon className="row-span-2 self-end" style={{ width: '210px', height: '210px' }} />
-        <div
-          className="w-4/5 row-span-1 font-semibold tracking-finnieSpacing-wider text-white"
-          style={{ fontSize: '35px', lineHeight: '41px' }}
-        >
-          Welcome to the Finnie Wallet
-        </div>
-        <div className="row-span-1 font-normal text-3xl leading-4  text-success">
-          Let’s get started.
-        </div>
-        <div className="w-4/5 row-span-3 font-normal text-lg leading-6 text-white">
-          To use the Finnie Wallet, add a key. This can be one you already have or you can make a
-          new one.
-        </div>
-      </div>
-      <div
-        className={clsx(
-          'w-3/5 h-full relative bg-gradient-to-r from-blue-300 to-indigo shadow-lg',
-          'flex flex-col items-center justify-evenly text-white'
-        )}
-      >
-        <WelcomeBackgroundTop className="absolute top-0 right-0" />
-        <WelcomeBackgroundBottom className="absolute bottom-0 left-0" />
-        <div
-          className="bg-blue-800 shadow-md rounded-finnie z-10 flex flex-col items-center justify-center"
-          style={{ width: '249px', height: '140px' }}
-        >
-          <CreateNewIcon style={{ width: '32px', height: '32px' }} />
-          <div className="mt-3 font-semibold text-base leading-4 text-center text-white">
-            Get a new key
-          </div>
-          <div className="mt-2 font-normal text-xs text-center tracking-finnieSpacing-wide text-white">
-            Start from the beginning.
-          </div>
-        </div>
-        <div
-          className="bg-blue-800 shadow-md rounded-finnie z-10 flex flex-col items-center justify-center"
-          style={{ width: '249px', height: '140px' }}
-        >
-          <ImportSeedphraseIcon style={{ width: '32px', height: '28px' }} />
-          <div className="mt-3 font-semibold text-base leading-4 text-center text-white">
-            Import with a seed phrase
-          </div>
-          <div className="mt-2 font-normal text-xs text-center tracking-finnieSpacing-wide text-white w-9/12">
-            Import an existing key using a 12-word recovery phrase.
-          </div>
-        </div>
-        <div
-          className="bg-blue-800 shadow-md rounded-finnie z-10 flex flex-col items-center justify-center"
-          style={{ width: '249px', height: '140px' }}
-        >
-          <ImportKeyIcon style={{ width: '32px', height: '28px' }} />
-          <div className="mt-3 font-semibold text-base leading-4 text-center text-white">
-            Import a private key
-          </div>
-          <div className="mt-2 font-normal text-xs text-center tracking-finnieSpacing-wide text-white w-8/12">
-            Import an existing key by uploading a .JSON file.
-          </div>
-        </div>
-      </div>
+      <NavBar step={step} setStep={setStep} />
+      <Content step={step} setStep={setStep} />
     </div>
   )
 }
