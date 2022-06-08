40f786756cf59b939a3fb77f056c32a6a7cc47da
Dat Huynh <dat.huynh@eastagile.com>
2022-06-08 17:10:31 +0700

    [b] gkr - Empty gallery needs a placeholder card

--- src/options/pages/Gallery/Gallery.js
+++ src/options/pages/Gallery/Gallery.js
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import isEmpty from 'lodash/isEmpty'
 
 import NFTCard from 'finnie-v2/components/NFTCard'
 import { GalleryContext } from 'options/galleryContext'
+import CreateIcon from 'img/v2/create-icon.svg'
 
 import './index.css'
 
@@ -38,9 +41,44 @@ const Gallery = () => {
   return (
     <div id="gallery" className="w-full flex justify-center items-center" onScroll={handleScroll}>
       <div className="h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-5 place-items-start">
-        {displayingNfts?.map((nft) => (
-          <NFTCard nft={nft} key={nft.txId} />
-        ))}
+        {!isEmpty(displayingNfts) ? (
+          displayingNfts.map((nft) => <NFTCard nft={nft} key={nft.txId} />)
+        ) : (
+          <Link
+            to={`/create-nft`}
+            className="relative text-white rounded bg-blue-800 w-46.75 h-72 pt-1.75 px-1.75"
+          >
+            <div
+              className="flex flex-col justify-center items-center w-full bg-blue-400 bg-opacity-40 rounded border border-dashed border-trueGray-100"
+              style={{ height: '163px' }}
+            >
+              <CreateIcon className="w-8.5 h-8.5" />
+              <div
+                className="text-white text-sm leading-6 tracking-finnieSpacing-wide mt-4 w-32 text-center"
+                style={{ width: '115px' }}
+              >
+                Create an Atomic NFT
+              </div>
+            </div>
+            <div
+              className="bg-blue-400 bg-opacity-25 rounded-3xl mt-2 mb-6"
+              style={{ width: '151px', height: '16px' }}
+            />
+
+            <div
+              className="bg-blue-400 bg-opacity-25 rounded-3xl"
+              style={{ width: '99px', height: '12px' }}
+            />
+            <div
+              className="bg-blue-400 bg-opacity-25 rounded-3xl my-1"
+              style={{ width: '71px', height: '12px' }}
+            />
+            <div
+              className="bg-blue-400 bg-opacity-25 rounded-3xl"
+              style={{ width: '87px', height: '12px' }}
+            />
+          </Link>
+        )}
       </div>
     </div>
   )
