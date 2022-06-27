fbac992eff98f86e0f53cf82069ec4aefec46a30
Long Pham <long.pham@eastagile.com>
2022-06-27 22:07:14 +0700

    [c] wjj - check existing accounts on new onboarding flow
    https://app.clickup.com/t/2fmawjj

--- src/options/finnie-v2/pages/Onboarding/Content/CreatePassword.js
+++ src/options/finnie-v2/pages/Onboarding/Content/CreatePassword.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react'
 import isEmpty from 'lodash/isEmpty'
+import { useSelector } from 'react-redux'
 
 import WelcomeBackgroundTop from 'img/v2/onboarding/welcome-background-top.svg'
 import WelcomeBackgroundBottom from 'img/v2/onboarding/welcome-background-bottom.svg'
@@ -27,28 +28,40 @@ const CreatePassword = ({ step, setStep }) => {
   const [isAcceptTermService, setIsAcceptTermService] = useState(false)
   const [termServiceMessage, setTermServiceMessage] = useState('')
 
+  const accounts = useSelector((state) => state.accounts)
+
   const onClickContinue = () => {
+    if (!isEmpty(accounts)) {
+      // TODO - handle check password
+      setStep(step + 1)
+      return
+    }
+
     if (!isAcceptTermService) {
       setTermServiceMessage('Please accept the Terms of Service')
     } else {
       if (isValidPassword) setStep(step + 1)
     }
   }
+
   return (
     <div className="w-2/3 flex flex-col text-white self-start pl-23" style={{ width: '500px' }}>
       <WelcomeBackgroundTop className="absolute top-0 right-0" />
       <WelcomeBackgroundBottom className="absolute bottom-0 left-0" />
       <div className="font-normal text-base leading-6 text-left">
-        Create a password to secure Finnie.
+        {isEmpty(accounts)
+          ? 'Create a password to secure Finnie.'
+          : 'Re-enter your Finnie password so we can securely store your new key.'}
       </div>
       <InputField
         className="mt-5"
-        label="New Password"
+        label={isEmpty(accounts) ? 'New Password' : 'Password'}
         value={password}
         setValue={(e) => setPassword(e.target.value)}
         required={true}
         name="password"
         errorFinnie={
+          isEmpty(accounts) &&
           !isEmpty(passwordErrorMessage) &&
           passwordErrorMessage != VALIDATE_ERROR_MESSAGE.NOT_MATCH &&
           passwordErrorMessage
@@ -58,41 +71,49 @@ const CreatePassword = ({ step, setStep }) => {
         uppercase={false}
         passwordFinnie={true}
       />
-      <InputField
-        className="mt-7 mb-5"
-        label="Confirm Password"
-        value={confirmPassword}
-        setValue={(e) => setConfirmPassword(e.target.value)}
-        required={true}
-        name="password"
-        placeholder=""
-        errorFinnie={
-          passwordErrorMessage === VALIDATE_ERROR_MESSAGE.NOT_MATCH && passwordErrorMessage
-        }
-        uppercase={false}
-        passwordFinnie={true}
-      />
-      <div className="flex items-center justify-center">
-        <CheckBox
-          checked={isAcceptTermService}
-          onClick={() => setIsAcceptTermService((prev) => !prev)}
-          theme="dark"
-          className="w-5 h-5"
+      {isEmpty(accounts) && (
+        <InputField
+          className="mt-7"
+          label="Confirm Password"
+          value={confirmPassword}
+          setValue={(e) => setConfirmPassword(e.target.value)}
+          required={true}
+          name="password"
+          placeholder=""
+          errorFinnie={
+            passwordErrorMessage === VALIDATE_ERROR_MESSAGE.NOT_MATCH && passwordErrorMessage
+          }
+          uppercase={false}
+          passwordFinnie={true}
         />
-        <div className="flex ml-2.25 font-normal text-sm leading-6">
-          <div onClick={() => setIsAcceptTermService((prev) => !prev)}>I agree with the&nbsp;</div>
-          <a target="_blank" href={URL.TERM_OF_SERVICE} className="text-turquoiseBlue underline">
-            Terms of Service
-          </a>
+      )}
+      {isEmpty(accounts) && (
+        <div className="mt-5 flex items-center justify-center">
+          <CheckBox
+            checked={isAcceptTermService}
+            onClick={() => setIsAcceptTermService((prev) => !prev)}
+            theme="dark"
+            className="w-5 h-5"
+          />
+          <div className="flex ml-2.25 font-normal text-sm leading-6">
+            <div onClick={() => setIsAcceptTermService((prev) => !prev)}>
+              I agree with the&nbsp;
+            </div>
+            <a target="_blank" href={URL.TERM_OF_SERVICE} className="text-turquoiseBlue underline">
+              Terms of Service
+            </a>
+          </div>
         </div>
+      )}
+      <div className="text-red-finnie text-xs font-normal mt-2 text-center">
+        {termServiceMessage}
       </div>
-      <div className="text-red-finnie text-xs font-normal mt-2 text-left">{termServiceMessage}</div>
       <Button
         style={{ width: '240px', height: '42px' }}
         className="mt-3.5 text-base rounded mx-auto z-10"
         variant="white"
         text="Log In"
-        disabled={!isValidPassword}
+        disabled={!isValidPassword && isEmpty(accounts)}
         onClick={onClickContinue}
       />
     </div>
