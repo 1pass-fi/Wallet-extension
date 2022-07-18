fc4c5367a7a32e691743774e8778a9f645e1a2e7
Long Pham <long.pham@eastagile.com>
2022-07-18 14:50:00 +0700

    [f] - add activities filter by account
    https://app.clickup.com/t/2r5d35a

--- src/options/pages/Notifications/Activity.js
+++ src/options/pages/Notifications/Activity.js
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState, useContext } from 'react'
 import { useSelector } from 'react-redux'
 
 import ActivityRow from './ActivityRow'
+import DropDown from 'finnie-v2/components/DropDown'
 
 import storage from 'services/storage'
 import { popupAccount } from 'services/account'
@@ -10,23 +11,64 @@ import { GalleryContext } from 'options/galleryContext'
 
 const Activity = () => {
   const [activities, setActivities] = useState([])
+  const [accountOption, setAccountOption] = useState('ALL')
 
-  const { displayingAccount } = useContext(GalleryContext)
+  const accounts = useSelector((state) => state.accounts)
+
+  const accountOptions = [{ label: 'All Accounts', value: 'ALL' }].concat(
+    accounts.map((account) => ({
+      label: account.accountName,
+      value: account.address
+    }))
+  )
 
   useEffect(() => {
     const loadActivities = async () => {
-      const account = await popupAccount.getAccount({ address: displayingAccount.address })
-      const allActivities = await account.get.activities()
-      setActivities(allActivities)
+      if (accountOption !== 'ALL') {
+        const account = await popupAccount.getAccount({ address: accountOption })
+        const allActivities = await account.get.activities()
+        console.log('accountOption', accountOption, allActivities)
+        setActivities(allActivities)
+      } else {
+        let allActivities = []
+        const accounts = await popupAccount.getAllAccounts()
+        await Promise.all(
+          accounts.map(async (account) => {
+            const activities = await account.get.activities()
+            allActivities = allActivities.concat(activities)
+          })
+        )
+        allActivities.sort((a, b) => b.time - a.time)
+
+        console.log('accountOption ALL', allActivities)
+        setActivities(allActivities)
+      }
     }
     loadActivities()
-  }, [displayingAccount])
+  }, [accountOption])
 
   const columns = useMemo(() => ['Date', 'Action', 'From', 'To', 'Amount', ''], [])
+
+  const onSelectedAccountChanged = (address) => {
+    setAccountOption(address)
+  }
+
+  console.log('activities', activities)
 
   return (
     <div className="pt-4 text-white">
       <h1 className="text-32px font-semibold underline">Activity</h1>
+      <div className="text-sm leading-6 mt-3 mb-2.5">Select account to see activity:</div>
+      <div className="mb-1.5" style={{ width: '270px' }}>
+        <DropDown
+          options={accountOptions}
+          value={accountOption}
+          onChange={onSelectedAccountChanged}
+          variant="dark"
+          size="lg"
+          filterSupported={false}
+        />
+      </div>
       <table className="w-full text-left table-auto border-collapse">
         <thead>
           <tr className="text-base h-13.75 font-semibold border-b border-white px-4">
