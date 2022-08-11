3740babe27005cfd427ab4c9ebcd3ac4657ae498
Long Pham <long.pham@eastagile.com>
2022-08-11 16:00:29 +0700

    [f] 621 - Sunrise API hook
    https://app.clickup.com/t/2tmw621

---
+++ src/popup/components/SignTransaction/hooks/useSecurityStatus.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { get, isEmpty } from 'lodash'
+
+const useSecurityStatus = ({ setIsLoading, url }) => {
+  const [trustStat, setTrustStat] = useState(null)
+
+  const getTrustStat = async () => {
+    try {
+      setIsLoading(true)
+      setTrustStat(null)
+      if (isEmpty(url)) {
+        return
+      }
+      const { data } = await axios.get(`https://api.sunrise.wtf/api/v1/status?url=${url}`)
+      console.log('getTrustStat ', url, data)
+      setTrustStat(get(data, 'status.trust_score'))
+    } catch (error) {
+      console.log('Failed to get Trust stat for ', url, '. Error: ', error)
+      setTrustStat(null)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getTrustStat()
+  }, [url])
+
+  return trustStat
+}
+
+export default useSecurityStatus
