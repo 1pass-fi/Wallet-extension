6679567aa3a6842c4f8953a111274ae776a5e897
Dat Huynh <dat.huynh@eastagile.com>
2022-09-05 15:47:26 +0700

    [f] signTransaction function solana account

--- src/background/handlers/contentScriptEvents/controller/solanaSignTransaction.js
+++ src/background/handlers/contentScriptEvents/controller/solanaSignTransaction.js
@@ -1,21 +1,33 @@
+import { Transaction } from '@solana/web3.js'
+import base58 from 'bs58'
+
 export default async (payload, tab, next) => {
   try {
-    const {       
-      origin, 
-      favicon, 
-      url, 
-      hadPermission, 
+    const {
+      origin,
+      favicon,
+      url,
+      hadPermission,
       hasPendingRequest,
       siteAddressDictionary,
       activatedAddress,
-      connectedAddresses 
+      connectedAddresses
     } = tab
 
     // TODO Thuan Ngo: implement signTransaction functions
     // Take a look at controller: signAllTransactions
     console.log('SOLANA SIGN TRANSACTION...')
- 
-    next({ data: 'example_signed_transaction' })
+    const params = get(payload, 'data.params')
+    const encodeTransaction = get(params, 'transaction')
+
+    const credentials = await backgroundAccount.getCredentialByAddress(connectedAddresses)
+    const solTool = new SolanaTool(credentials)
+    const keypair = solTool.keypair
+
+    const transaction = Transaction.from(base58.decode(encodeTransaction))
+    transaction.sign(keypair)
+
+    next({ data: transaction })
   } catch (err) {
     console.error(err.mesage)
     next({ data: { status: 500, data: 'Solana signTransaction error' } })
