2dc0fc9b17bf86cbbc1a915a2670a920864bc018
Long Pham <long.pham@eastagile.com>
2022-10-17 11:45:46 +0700

    [c] - update get eth transaction receipt

--- src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethGetTransactionReceipt.js
+++ src/background/handlers/contentScriptEvents/controller/rpcRequestHandlers/ethGetTransactionReceipt.js
@@ -16,20 +16,12 @@ export default async (payload, tab, next) => {
     const network = ethers.providers.getNetwork(ethNetwork)
     const web3 = new ethers.providers.InfuraProvider(network, apiKey)
 
-    // const transactionReceipt = await web3.eth.getTransactionReceipt(transactionHash)
-    const transactionReceipt = await web3.getTransactionReceipt(transactionHash)
+    const transactionReceipt = await web3.waitForTransaction(transactionHash)
 
-    // transactionReceipt.blockNumber = web3.utils.toHex(transactionReceipt.blockNumber)
     transactionReceipt.blockNumber = ethers.utils.hexlify(transactionReceipt.blockNumber)
-    // transactionReceipt.cumulativeGasUsed = web3.utils.toHex(transactionReceipt.cumulativeGasUsed)
-    transactionReceipt.cumulativeGasUsed = ethers.utils.hexlify(
-      transactionReceipt.cumulativeGasUsed
-    )
-    // transactionReceipt.gasUsed = web3.utils.toHex(transactionReceipt.gasUsed)
+    transactionReceipt.cumulativeGasUsed = ethers.utils.hexlify(transactionReceipt.cumulativeGasUsed)
     transactionReceipt.gasUsed = ethers.utils.hexlify(transactionReceipt.gasUsed)
-    // transactionReceipt.status = web3.utils.toHex(transactionReceipt.status)
     transactionReceipt.status = ethers.utils.hexlify(transactionReceipt.status)
-    // transactionReceipt.transactionIndex = web3.utils.toHex(transactionReceipt.transactionIndex)
     transactionReceipt.transactionIndex = ethers.utils.hexlify(transactionReceipt.transactionIndex)
 
     console.log('transactionReceipt', transactionReceipt)
