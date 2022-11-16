d73c112589a5260c042cc7ac9deecda3ae7a964b
DatHG <dat.huynh@eastagile.com>
2022-11-16 19:01:30 +0700

    [f] - walletconnect - personal_sign controller

--- src/background/handlers/walletConnectEvents/controller/personalSign.js
+++ src/background/handlers/walletConnectEvents/controller/personalSign.js
@@ -1,12 +1,119 @@
+import { OS, REQUEST, WINDOW_SIZE } from 'constants/koiConstants'
+import { ethers } from 'ethers'
+import { get } from 'lodash'
+import { backgroundAccount } from 'services/account'
+import storage from 'services/storage'
+import ethereumUtils from 'utils/ethereumUtils'
+import { createWindow } from 'utils/extension'
+import { v4 as uuid } from 'uuid'
+
+function convertHexToUtf8(value) {
+  if (ethers.utils.isHexString(value)) {
+    return ethers.utils.toUtf8String(value)
+  }
+  return value
+}
+
 export default async (payload, metadata, next) => {
   try {
     console.log('personal sign')
     console.log('payload', payload)
     console.log('metadata', metadata)
 
-    const exampleSignature = '0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b'
-    /* TODO walletconnect: Implement personal sign */
-    next({ data: exampleSignature })
+    const params = get(payload, 'params')
+    const message = convertHexToUtf8(params[0])
+
+    /* Show popup */
+    const screenWidth = screen.availWidth
+    const screenHeight = screen.availHeight
+    const os = window.localStorage.getItem(OS)
+    let windowData = {
+      url: chrome.extension.getURL('/popup.html'),
+      focused: true,
+      type: 'popup'
+    }
+    if (os == 'win') {
+      windowData = {
+        ...windowData,
+        height: WINDOW_SIZE.WIN_HEIGHT,
+        width: WINDOW_SIZE.WIN_WIDTH,
+        left: Math.round((screenWidth - WINDOW_SIZE.WIN_WIDTH) / 2),
+        top: Math.round((screenHeight - WINDOW_SIZE.WIN_HEIGHT) / 2)
+      }
+    } else {
+      windowData = {
+        ...windowData,
+        height: WINDOW_SIZE.MAC_HEIGHT,
+        width: WINDOW_SIZE.MAC_WIDTH,
+        left: Math.round((screenWidth - WINDOW_SIZE.MAC_WIDTH) / 2),
+        top: Math.round((screenHeight - WINDOW_SIZE.MAC_HEIGHT) / 2)
+      }
+    }
+
+    const requestId = uuid()
+    const requestPayload = {
+      requestId,
+      isEthereum: true,
+      requestPayload: {
+        message: message
+      }
+    }
+
+    createWindow(windowData, {
+      beforeCreate: async () => {
+        chrome.browserAction.setBadgeText({ text: '1' })
+        chrome.runtime.onMessage.addListener(async function (popupMessage, sender, sendResponse) {
+          if (popupMessage.requestId === requestId) {
+            const approved = popupMessage.approved
+            if (approved) {
+              try {
+                const defaultEthereumAddress = await storage.setting.get.activatedEthereumAccountAddress()
+                const credential = await backgroundAccount.getCredentialByAddress(
+                  defaultEthereumAddress
+                )
+
+                const provider = await storage.setting.get.ethereumProvider()
+                const { ethersProvider, wallet } = ethereumUtils.initEthersProvider(
+                  provider,
+                  credential.key
+                )
+
+                const signer = wallet.connect(ethersProvider)
+                const msgSig = await signer.signMessage(message)
+
+                const verifiedAddress = ethers.utils.verifyMessage(message, msgSig)
+
+                console.log('verifiedAddress', verifiedAddress)
+
+                if (verifiedAddress === credential.address) {
+                  next({ data: msgSig })
+                } else {
+                  next({ error: { code: 4003, data: 'Fail to verify the signature' } })
+                }
+
+                chrome.runtime.sendMessage({ requestId, finished: true })
+              } catch (err) {
+                console.error('Personal sign error:', err.message)
+                chrome.runtime.sendMessage({ requestId, finished: true })
+                next({ error: { code: 4001, data: err.message } })
+              }
+            } else {
+              next({ error: { code: 4001, data: 'User rejected request' } })
+            }
+          }
+        })
+
+        await storage.generic.set.pendingRequest({
+          type: REQUEST.PERSONAL_SIGN,
+          data: requestPayload
+        })
+      },
+      afterClose: async () => {
+        chrome.browserAction.setBadgeText({ text: '' })
+        next({ error: 'User cancelled sign message.' })
+        await storage.generic.set.pendingRequest({})
+      }
+    })
   } catch (err) {
     next({ error: err.message })
   }
