76496dca5dc244a43b0dc4cea0bad2d31da71fb4
DatHG <dat.huynh@eastagile.com>
2022-11-17 15:34:10 +0700

    [f] - walletconnect - eth_sign controller

---
+++ src/background/handlers/walletConnectEvents/controller/ethSign.js
@@ -0,0 +1,112 @@
+import { concatSig } from '@metamask/eth-sig-util'
+import { OS, REQUEST, WINDOW_SIZE } from 'constants/koiConstants'
+import { ecsign, stripHexPrefix } from 'ethereumjs-util'
+import { ethers } from 'ethers'
+import { get } from 'lodash'
+import { backgroundAccount } from 'services/account'
+import storage from 'services/storage'
+import ethereumUtils from 'utils/ethereumUtils'
+import { createWindow } from 'utils/extension'
+import { v4 as uuid } from 'uuid'
+
+export function convertHexToUtf8(value) {
+  if (ethers.utils.isHexString(value)) {
+    return ethers.utils.toUtf8String(value)
+  }
+  return value
+}
+
+export default async (payload, metadata, next) => {
+  try {
+    console.log('eth sign')
+    console.log('payload', payload)
+    console.log('metadata', metadata)
+
+    const params = get(payload, 'params')
+    const message = params[1]
+
+    /* Show popup */
+    const screenWidth = screen.availWidth
+    const screenHeight = screen.availHeight
+    const os = window.localStorage.getItem(OS)
+    let windowData = {
+      url: chrome.extension.getURL('/popup.html'),
+      focused: true,
+      type: 'popup'
+    }
+    if (os == 'win') {
+      windowData = {
+        ...windowData,
+        height: WINDOW_SIZE.WIN_HEIGHT,
+        width: WINDOW_SIZE.WIN_WIDTH,
+        left: Math.round((screenWidth - WINDOW_SIZE.WIN_WIDTH) / 2),
+        top: Math.round((screenHeight - WINDOW_SIZE.WIN_HEIGHT) / 2)
+      }
+    } else {
+      windowData = {
+        ...windowData,
+        height: WINDOW_SIZE.MAC_HEIGHT,
+        width: WINDOW_SIZE.MAC_WIDTH,
+        left: Math.round((screenWidth - WINDOW_SIZE.MAC_WIDTH) / 2),
+        top: Math.round((screenHeight - WINDOW_SIZE.MAC_HEIGHT) / 2)
+      }
+    }
+
+    const requestId = uuid()
+    const requestPayload = {
+      requestId,
+      isEthereum: true,
+      requestPayload: {
+        message: message
+      }
+    }
+
+    createWindow(windowData, {
+      beforeCreate: async () => {
+        chrome.browserAction.setBadgeText({ text: '1' })
+        chrome.runtime.onMessage.addListener(async function (popupMessage, sender, sendResponse) {
+          if (popupMessage.requestId === requestId) {
+            const approved = popupMessage.approved
+            if (approved) {
+              try {
+                const defaultEthereumAddress = await storage.setting.get.activatedEthereumAccountAddress()
+                const credential = await backgroundAccount.getCredentialByAddress(
+                  defaultEthereumAddress
+                )
+
+                const msgSig = ecsign(
+                  Buffer.from(stripHexPrefix(message), 'hex'),
+                  Buffer.from(stripHexPrefix(credential.key), 'hex')
+                )
+
+                const rawMsgSig = concatSig(msgSig.v, msgSig.r, msgSig.s)
+
+                next({ data: rawMsgSig })
+
+                chrome.runtime.sendMessage({ requestId, finished: true })
+              } catch (err) {
+                console.error('ETH sign error:', err.message)
+                chrome.runtime.sendMessage({ requestId, finished: true })
+                next({ error: { code: 4001, data: err.message } })
+              }
+            } else {
+              next({ error: { code: 4001, data: 'User rejected request' } })
+            }
+          }
+        })
+
+        await storage.generic.set.pendingRequest({
+          type: REQUEST.ETH_SIGN,
+          data: requestPayload
+        })
+      },
+      afterClose: async () => {
+        chrome.browserAction.setBadgeText({ text: '' })
+        next({ error: 'User cancelled sign message.' })
+        await storage.generic.set.pendingRequest({})
+      }
+    })
+  } catch (err) {
+    next({ error: err.message })
+  }
+}
