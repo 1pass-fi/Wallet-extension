22802f827c56afb81283f2d77de54d76e6bd52a9
Long Pham <long.pham@eastagile.com>
2022-12-02 16:36:11 +0700

    [c] add background
    https://gitlab.com/koii-network/finnie/-/issues/414

--- src/popup/pages/WalletConnect/InputUri.js
+++ src/popup/pages/WalletConnect/InputUri.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { setIsLoading } from 'actions/loading'
 import BackIcon from 'img/wallet-connect/back-icon.svg'
+import BackgroundLeft from 'img/wallet-connect/bg-left.svg'
+import BackgroundRight from 'img/wallet-connect/bg-right.svg'
 import GlobeIcon from 'img/wallet-connect/globe-big-icon.svg'
 import get from 'lodash/get'
 import { setError } from 'popup/actions/error'
@@ -21,16 +23,21 @@ const InputUri = ({ setPage, setProposal }) => {
 
   const validateProposal = (proposal) => {
     try {
-      const validChains = ['eip155:1', 'eip155:5', 'solana:8E9rvCKLFQia2Y35HXjjpWzj8weVo44K', 'solana:4sGjMW1sUnHzSxGspuhpqLDx6wiyjNtZ']
-  
-      const namespaces = (get(proposal, 'params.requiredNamespaces'))
+      const validChains = [
+        'eip155:1',
+        'eip155:5',
+        'solana:8E9rvCKLFQia2Y35HXjjpWzj8weVo44K',
+        'solana:4sGjMW1sUnHzSxGspuhpqLDx6wiyjNtZ'
+      ]
+
+      const namespaces = get(proposal, 'params.requiredNamespaces')
       let chains = []
-      Object.keys(namespaces).forEach(chainName => {
+      Object.keys(namespaces).forEach((chainName) => {
         const _chains = namespaces[chainName]?.chains || []
         chains = [...chains, ..._chains]
       })
 
-      return chains.every(chain => validChains.includes(chain)) && chains?.length === 1
+      return chains.every((chain) => validChains.includes(chain)) && chains?.length === 1
     } catch (err) {
       console.error(err)
       return false
@@ -52,7 +59,7 @@ const InputUri = ({ setPage, setProposal }) => {
       })
       await walletConnect.pair(uri)
     } catch (err) {
-      dispatch(setError(err?.message))      
+      dispatch(setError(err?.message))
     }
     dispatch(setIsLoading(false))
   }
@@ -63,14 +70,31 @@ const InputUri = ({ setPage, setProposal }) => {
 
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <div onClick={handleGoBack} className='fixed top-16 left-3 cursor-pointer'><BackIcon /></div>
-      <GlobeIcon />
-      <div className=' mt-5 mb-7 text-indigo text-base font-semibold'>Wallet Connect</div>
-      <div>
-        <div style={{height:'24px'}} className='flex items-center text-indigo text-sm'>Enter wallet connect URL</div>
-        <input onChange={e => setUri(e.target.value)} style={{width:'360px',height:'36px'}} className='mt-1 bg-purplelight-100 color-purplelight-100 pl-4 rounded-md' placeholder='URI'/>
+      <BackgroundLeft className="fixed top-0 left-0" />
+      <BackgroundRight className="fixed top-0 right-0" />
+      <div onClick={handleGoBack} className="fixed top-16 left-3 cursor-pointer">
+        <BackIcon />
       </div>
-      <button onClick={handleConnect} style={{width:'160px',height:'38px'}} className='bg-blue-800 text-white text-base mt-7'>Connect</button>
+      <GlobeIcon />
+      <div className="mt-5 mb-7 text-indigo text-base font-semibold">Wallet Connect</div>
+      <div>
+        <div style={{ height: '24px' }} className="flex items-center text-indigo text-sm">
+          Enter wallet connect URL
+        </div>
+        <input
+          onChange={(e) => setUri(e.target.value)}
+          style={{ width: '360px', height: '36px' }}
+          className="mt-1 bg-purplelight-100 color-purplelight-100 pl-4 rounded-md"
+          placeholder="Type here"
+        />
+      </div>
+      <button
+        onClick={handleConnect}
+        style={{ width: '160px', height: '38px' }}
+        className="bg-blue-800 text-white text-base mt-7 rounded-sm"
+      >
+        Connect
+      </button>
     </div>
   )
 }
