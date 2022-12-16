6a55d9d1f0f289b934a8a743dbe256226495047d
Minh Vu <minh.vu@eastagile.com>
2022-12-16 15:48:39 +0700

    [c]  - ChromeStorage unit tests

---
+++ src/services/storage/ChromeStorage.test.js
@@ -0,0 +1,89 @@
+import sinon from 'sinon'
+
+import { ChromeStorage } from './ChromeStorage'
+
+describe('ChromeStorage class', () => {
+  let chromeStorage
+
+  const TEST_KEY = 'user'
+  const TEST_VALUE = ['userOne', 'userTwo']
+
+  beforeAll(async (done) => {
+    chromeStorage = new ChromeStorage()
+    await chromeStorage._setChrome(TEST_KEY, TEST_VALUE)
+    done()
+  })
+
+  afterAll(async (done) => {
+    await chrome.storage.local.clear()
+    done()
+  })
+
+  describe('Get method', () => {
+    describe('Failed to get data', () => {
+      it('returns null when receive non-string input', async () => {
+        const result = await chromeStorage._getChrome([])
+        expect(result).toBe(null)
+      })
+  
+      it('returns null when key not match', async () => {
+        const result = await chromeStorage._getChrome('non_existing_key')
+        expect(result).toBe(null)
+      })
+
+      it('returns null when chrome api throws an error', async () => {
+        const stub = sinon.stub(chrome.storage.local, 'get').throws()
+        const result = await chromeStorage._getChrome()
+        expect(result).toBe(null)
+
+        stub.restore()
+      }) 
+    })
+
+    describe('Succeeded to get data', () => {
+      it ('gets correct data', async () => {
+        const result = await chromeStorage._getChrome(TEST_KEY)
+        expect(result).toEqual(TEST_VALUE)
+      })
+    })
+  
+  })
+
+  describe('Set method', () => {
+    describe('Failed to set data', () => {
+      it('skips when receive invalid input', async () => {
+        const invalidKey = []
+        const value = 1
+
+        await chromeStorage._setChrome(invalidKey, value)
+        const result = await chromeStorage._getChrome(invalidKey)
+        expect(result).toBe(null)
+      })
+    })
+
+    describe('Succeeded to set data', () => {
+      it('sets correct data', async () => {
+        const newKey = 'new_key'
+        const newValue = 'new_value'
+  
+        await chromeStorage._setChrome(newKey, newValue)
+        const result = await chromeStorage._getChrome(newKey)
+  
+        expect(result).toEqual(newValue)
+      })
+    })
+  })
+
+  describe('Remove method', () => {
+    it('removes correct key', async () => {
+      let result = await chromeStorage._getChrome(TEST_KEY)
+
+      expect(result).toEqual(TEST_VALUE)
+
+      await chromeStorage._removeChrome(TEST_KEY)
+      result = await chromeStorage._getChrome(TEST_KEY)
+
+      expect(result).toEqual(null)
+    })
+  })
+})
