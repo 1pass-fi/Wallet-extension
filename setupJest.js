e83c67827d0b397898f09de222319813c752df88
DatHG <dat.huynh@eastagile.com>
2022-12-22 11:24:29 +0700

    [c] - Config jest-dom for component testing

--- setupJest.js
+++ setupJest.js
@@ -2,3 +2,5 @@ require('@babel/polyfill')
 
 global.TextDecoder = require('text-encoding').TextDecoder
 global.TextEncoder = require('text-encoding').TextEncoder
+
+require('@testing-library/jest-dom')
