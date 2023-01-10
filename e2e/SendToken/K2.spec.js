dc3ad8c82b9f8d084a73b41f459b724ddf0295c3
Long Pham <long.pham@eastagile.com>
2023-01-10 11:53:58 +0700

    [t] - add send custom K2 token e2e test
    https://gitlab.com/koii-network/finnie/-/issues/435
    
    [t] - update bootstrap

--- e2e/SendToken/K2.spec.js
+++ e2e/SendToken/K2.spec.js
@@ -67,12 +67,91 @@ describe('Send token via K2 network', () => {
     await sendButton.click()
 
     // /* TRANSACTION RECEIPT */
+    await extPage.waitForTimeout(1000)
     const okButton = await extPage.waitForSelector('a[role="button"]')
     await okButton.click()
 
     await extPage.waitForTimeout(1000)
   }, 500000)
 
+  it('should successfully to send custom tokens', async () => {
+    await extPage.bringToFront()
+
+    /* IMPORT CUSTOM TOKEN */
+    let goToImportToken = await extPage.waitForSelector(`[data-testid="Tokens"]`)
+    await goToImportToken.click()
+
+    let importTokenButton = await extPage.waitForSelector(`[data-testid="import-token-button"]`)
+    await importTokenButton.click()
+
+    let searchInputField = await extPage.waitForSelector(
+      `[data-testid="search-custom-token-input"]`
+    )
+
+    await searchInputField.type('WI')
+
+    const wibuTokenOption = await extPage.waitForSelector(`[data-testid="WIBU"]`)
+    await wibuTokenOption.click()
+
+    let selectAccountCheckbox = await extPage.waitForSelector('div[role="checkbox"]')
+    await selectAccountCheckbox.click()
+
+    let [confirmButton] = await extPage.$x('//button[text()="Confirm"]')
+    await confirmButton.click()
+
+    // TODO DatH - wait for TRANSACTION CONFIRM MODAL data
+    await extPage.waitForTimeout(10000)
+
+    const goToSendButton = await extPage.waitForSelector('a[role="button"]')
+    await goToSendButton.click()
+
+    /* SEND TOKEN FORM */
+    const tokenDropdown = await extPage.waitForSelector('[role="button"]')
+    // console.log('tokenDropdown', tokenDropdown)
+    await tokenDropdown.click()
+
+    const tokenOption = await extPage.waitForSelector(`[data-testid="WIBU"]`)
+    await tokenOption.click()
+    await extPage.waitForTimeout(1000)
+
+    const amountInputField = await extPage.waitForSelector(`[data-testid="input-send-amount"]`)
+    await amountInputField.type('0.0001')
+
+    const recipientAddressInputField = await extPage.waitForSelector(
+      `[data-testid="recipient-address"]`
+    )
+    await recipientAddressInputField.type('32Dz2b9UtGymREov4EzkBsn52E6UaXHRLeECwXxEzxJ7')
+
+    const sendTokensButton = await extPage.waitForSelector(`[data-testid="send-tokens-button"]`)
+    await sendTokensButton.click()
+
+    // TODO DatH - wait for TRANSACTION CONFIRM MODAL data
+    await extPage.waitForTimeout(5000)
+
+    /* TRANSACTION CONFIRM MODAL */
+    const senderConfirm = await extPage.waitForSelector(`[data-testid="tx-confirm-sender"]`)
+    const sender = await senderConfirm.evaluate((el) => el.textContent)
+
+    expect(sender).toBe('32Dz2b9UtGymREov4EzkBsn52E6UaXHRLeECwXxEzxJ7')
+
+    const recipientConfirm = await extPage.waitForSelector(`[data-testid="tx-confirm-recipient"]`)
+    const recipient = await recipientConfirm.evaluate((el) => el.textContent)
+    expect(recipient).toBe('32Dz2b9UtGymREov4EzkBsn52E6UaXHRLeECwXxEzxJ7')
+
+    const amountConfirm = await extPage.waitForSelector(`[data-testid="tx-confirm-amount"]`)
+    const amount = await amountConfirm.evaluate((el) => el.textContent)
+    expect(amount).toBe('0.0001 WIBU')
+
+    const [sendButton] = await extPage.$x('//button[text()="Send"]')
+    await sendButton.click()
+
+    // /* TRANSACTION RECEIPT */
+    await extPage.waitForTimeout(1000)
+    const okButton = await extPage.waitForSelector('a[role="button"]')
+    await okButton.click()
+
+    await extPage.waitForTimeout(1000)
+  }, 500000)
 
   afterAll(async () => {
     await context.closePages()
