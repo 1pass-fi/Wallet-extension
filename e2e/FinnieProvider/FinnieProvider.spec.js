f95e1decafe30040953486bed875f91e9d847355
Minh Vu <minh.vu@eastagile.com>
2023-01-24 16:49:14 +0700

    [c] - e2e test finnie provider - k2 connect + sign

--- e2e/FinnieProvider/FinnieProvider.spec.js
+++ e2e/FinnieProvider/FinnieProvider.spec.js
@@ -18,16 +18,16 @@ describe('Finnie Provider', () => {
     optionPage = context.optionPage
     testDappPage = await context.launchTestDappPage()
 
-    await Automation.importWallet(optionPage, TYPE.SOLANA)
   }, 50000)
-
+  
   afterEach(async () => {
     await context.closePages()
     return true
   })
-
+  
   describe('Solana provider', () => {
     it ('should call solana methods successfully', async () => {
+      await Automation.importWallet(optionPage, TYPE.SOLANA)
       const walletAddress = '9cGCJvVacp5V6xjeshprS3KDN3e5VwEUszHmxxaZuHmJ'
       testDappPage.bringToFront()
 
@@ -107,6 +107,85 @@ describe('Finnie Provider', () => {
   
       /* Expect to receive empty string after disconnect */
       expect(connectSolanaResult).toEqual('')
+    }, 50000)
+  })
+
+  describe('K2 provider', () => {
+    it ('should call k2 methods successfully', async () => {
+      await Automation.importWallet(optionPage, TYPE.K2)
+      const walletAddress = '32Dz2b9UtGymREov4EzkBsn52E6UaXHRLeECwXxEzxJ7'
+      testDappPage.bringToFront()
+
+      /* Query check code injection button */
+      const codeInjectedButton = await testDappPage.$(`[data-testid="code-injected-button"]`)
+      await codeInjectedButton.click()
+  
+      /* Query code injection result */
+      let codeInjectedResult = await testDappPage.$(`[data-testid="code-injected-result"]`)
+      codeInjectedResult = await codeInjectedResult.evaluate(el => el.textContent)
+  
+      /* Expect finnie provider has been injected successfully */
+      expect(codeInjectedResult).toEqual('true')
+  
+      /* Query connect solana button */
+      const connectK2Button = await testDappPage.$(`[data-testid="connect-k2-button"]`)
+      await connectK2Button.click()
+  
+      testDappPage.bringToFront()
+      await sleep()
+  
+      /* Open popup page */
+      let extPage = await context.launchExtPage()
+
+      /* Query Select Wallet button */
+      const selectWalletButton = await extPage.$(`[data-testid="select-wallet-button"]`)
+      await selectWalletButton.click()
+  
+      /* Query Connect button */
+      const connectButton = await extPage.$(`[data-testid="connect-button"]`)
+      await connectButton.click()
+      sleep(1000)
+  
+      /* Focus dapp */
+      testDappPage.bringToFront()
+      await sleep()
+  
+      /* Query k2 connect result */
+      let connectK2Result = await testDappPage.waitForSelector(`[data-testid="connect-k2-result"]`)
+      connectK2Result = await connectK2Result.evaluate(el => el.textContent)
+  
+      /* Expect to receive correct wallet address */
+      expect(connectK2Result).toEqual(walletAddress)
+
+      /* Sign transaction */
+      await testDappPage.bringToFront()
+      const signTransactionK2Button = await testDappPage.$(`[data-testid="sign-transaction-k2"]`)
+      await sleep(1000)
+      await signTransactionK2Button.click()
+      await sleep(5000)
+      extPage = await context.launchExtPage()
+      const signButton = await extPage.$(`[data-testid="sign-button"]`)
+      await signButton.click()
+      await sleep(1000)
+      extPage.close()
+
+      /* Expect sign result to be correct wallet address */
+      await testDappPage.bringToFront()
+      let signK2Result = await testDappPage.$(`[data-testid="sign-k2-result"]`)
+      signK2Result = await signK2Result.evaluate(el => el.textContent)
+      expect(signK2Result).toEqual(walletAddress)
+
+      /* Query Disconnect button */
+      const disconnectK2Button = await testDappPage.$(`[data-testid="disconnect-k2-button"]`)
+      await disconnectK2Button.click()
+      await sleep()
+  
+      /* Query connect solana result */
+      connectK2Result = await testDappPage.waitForSelector(`[data-testid="connect-k2-result"]`)
+      connectK2Result = await connectK2Result.evaluate(el => el.textContent)
+  
+      /* Expect to receive empty string after disconnect */
+      expect(connectK2Result).toEqual('')
     }, 50000)
   })
 })
--- test-dapp/src/App.js
+++ test-dapp/src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [connectSolanaResult, setConnectSolanaResult] = useState(null)
   const [signSolanaResult, setSignSolanaResult] = useState(null)
   const [connectK2Result, setConnectK2Result] = useState(null)
+  const [signK2Result, setSignK2Result] = useState(null)
   const [connectEthereumResult, setConnectEthereumResult] = useState(null)
 
   const signTransactionSolana = async () => {
@@ -41,6 +42,33 @@ function App() {
 
     const signedPublicKey = transaction.signatures[0].publicKey.toString()
     setSignSolanaResult(signedPublicKey)
+  }
+
+  const signTransactionK2 = async () => {
+    window.Buffer = Buffer
+    const connection = new Connection(clusterApiUrl('devnet'))
+    const blockHash = await connection.getLatestBlockhash()
+    const feePayer = window.k2.publicKey
+
+    const transaction = new Transaction()
+    transaction.recentBlockhash = blockHash.blockhash
+    transaction.feePayer = feePayer
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: window.k2.publicKey,
+        toPubkey: new window.solanaWeb3.PublicKey('EKRyV8Ku1P9WBP65LBzNTmJbbes9dJBSV2qKq93EwNuf'),
+        lamports: 0
+      })
+    )
+
+    const signed = await window.k2.signTransaction(transaction)
+
+    console.log('signed:', signed)
+    console.log('transaction', transaction)
+
+    const signedPublicKey = transaction.signatures[0].publicKey.toString()
+    setSignK2Result(signedPublicKey)
   }
 
   const checkCodeInjection = () => {
@@ -74,8 +102,11 @@ function App() {
     }
   }
 
-  const disconnectK2 = () => {
-    window.k2.disconnect()
+  const disconnectK2 = async () => {
+    await window.k2.disconnect()
+    const isConnected = window.k2.isConnected
+
+    if (!isConnected) setConnectK2Result("")
   }
 
   const connectEthereum = async () => {
@@ -111,13 +142,15 @@ function App() {
       <button data-testid="connect-solana-button" onClick={connectSolana}>Connect Solana</button>
       <button data-testid="disconnect-solana-button" onClick={disconnectSolana}>Disconnect Solana</button>
       <button data-testid="sign-transaction-solana" onClick={signTransactionSolana}>Sign Transaction Solana</button>
-      <div>Connect K2 result: {connectK2Result}</div>
-      <button onClick={connectK2}>Connect K2</button>
-      <button onClick={disconnectK2}>Disconnect K2</button>
+      <div>Connect K2 result: <span data-testid="connect-k2-result">{connectK2Result}</span></div>
+      <div>Sign K2 result: <span data-testid="sign-k2-result">{signK2Result}</span></div>
+      <button data-testid="connect-k2-button" onClick={connectK2}>Connect K2</button>
+      <button data-testid="disconnect-k2-button" onClick={disconnectK2}>Disconnect K2</button>
+      <button data-testid="sign-transaction-k2" onClick={signTransactionK2}>Sign Transaction K2</button>
       <div>Connect Ethereum result: {connectEthereumResult}</div>
       <button onClick={connectEthereum}>Connect Ethereum</button>
       <button onClick={disconnectEthereum}>Disconnect Ethereum</button>
-    </div>
+    </div>  
   );
 }
 
