4b7330938ff603d380bac79331f37b9e92397c54
DatHG <dat.huynh@eastagile.com>
2023-02-08 14:53:41 +0700

    [c] Switch to koii sdk

--- src/services/ethereum/index.js
+++ src/services/ethereum/index.js
@@ -1,98 +1,101 @@
-import { generateMnemonic, mnemonicToSeedSync } from 'bip39'
-import { ETH_NETWORK_PROVIDER } from 'constants/koiConstants'
-import hdkey from 'ethereumjs-wallet/dist/hdkey'
-import { ethers } from 'ethers'
+// import { generateMnemonic, mnemonicToSeedSync } from 'bip39'
+// import { ETH_NETWORK_PROVIDER } from 'constants/koiConstants'
+// import hdkey from 'ethereumjs-wallet/dist/hdkey'
+// import { ethers } from 'ethers'
 
-export class EthereumTool {
-  #provider
-  #web3
-  constructor(provider) {
-    this.#provider = provider || ETH_NETWORK_PROVIDER.MAINNET
+// export class EthereumTool {
+//   #provider
+//   #web3
+//   constructor(provider) {
+//     this.#provider = provider || ETH_NETWORK_PROVIDER.MAINNET
 
-    const clarifyEthereumProvider = (ethProvider) => {
-      try {
-        let ethNetwork, apiKey
-        const providerArray = ethProvider.split('/')
-        apiKey = providerArray[4]
-        ethNetwork = providerArray[2].split('.')[0]
-        return { ethNetwork, apiKey }
-      } catch (err) {
-        console.error('Failed to clarify Ethereum Provider - error: ', err.message)
-        return { ethNetwork: 'mainnet', apiKey: 'f811f2257c4a4cceba5ab9044a1f03d2' }
-      }
-    }
+//     const clarifyEthereumProvider = (ethProvider) => {
+//       try {
+//         let ethNetwork, apiKey
+//         const providerArray = ethProvider.split('/')
+//         apiKey = providerArray[4]
+//         ethNetwork = providerArray[2].split('.')[0]
+//         return { ethNetwork, apiKey }
+//       } catch (err) {
+//         console.error('Failed to clarify Ethereum Provider - error: ', err.message)
+//         return { ethNetwork: 'mainnet', apiKey: 'f811f2257c4a4cceba5ab9044a1f03d2' }
+//       }
+//     }
 
-    const { ethNetwork, apiKey } = clarifyEthereumProvider(this.#provider)
+//     const { ethNetwork, apiKey } = clarifyEthereumProvider(this.#provider)
 
-    const network = ethers.providers.getNetwork(ethNetwork)
+//     const network = ethers.providers.getNetwork(ethNetwork)
 
-    this.#web3 = new ethers.providers.InfuraProvider(network, apiKey)
-    this.key = null
-    this.address = null
-  }
+//     this.#web3 = new ethers.providers.InfuraProvider(network, apiKey)
+//     this.key = null
+//     this.address = null
+//   }
 
-  web3() {
-    return this.#web3
-  }
+//   web3() {
+//     return this.#web3
+//   }
 
-  getCurrentNetWork() {
-    return this.#provider
-  }
+//   getCurrentNetWork() {
+//     return this.#provider
+//   }
 
-  createNewWallet() {
-    const seedPhrase = this.#generateMnemonic()
+//   createNewWallet() {
+//     const seedPhrase = this.#generateMnemonic()
 
-    const createdWallet = this.#getWalletFromSeedPhrase(seedPhrase)
-    this.key = createdWallet.privateKey
-    this.address = createdWallet.address
-    return seedPhrase
-  }
+//     const createdWallet = this.#getWalletFromSeedPhrase(seedPhrase)
+//     this.key = createdWallet.privateKey
+//     this.address = createdWallet.address
+//     return seedPhrase
+//   }
 
-  importWallet(payload, type) {
-    let wallet
-    if (type === 'key') {
-      // wallet = this.#web3.eth.accounts.privateKeyToAccount(payload)
-      wallet = new ethers.Wallet(payload, this.#web3)
-    } else {
-      wallet = this.#getWalletFromSeedPhrase(payload)
-    }
-    this.key = wallet.privateKey
-    this.address = wallet.address
+//   importWallet(payload, type) {
+//     let wallet
+//     if (type === 'key') {
+//       // wallet = this.#web3.eth.accounts.privateKeyToAccount(payload)
+//       wallet = new ethers.Wallet(payload, this.#web3)
+//     } else {
+//       wallet = this.#getWalletFromSeedPhrase(payload)
+//     }
+//     this.key = wallet.privateKey
+//     this.address = wallet.address
 
-    return wallet
-  }
+//     return wallet
+//   }
 
-  async getBalance() {
-    return this.#web3.getBalance(this.address)
-  }
+//   async getBalance() {
+//     return this.#web3.getBalance(this.address)
+//   }
 
-  async getTransactionStatus(txHash) {
-    return this.#web3.getTransactionReceipt(txHash)
-  }
+//   async getTransactionStatus(txHash) {
+//     return this.#web3.getTransactionReceipt(txHash)
+//   }
 
-  /*
-    PRIVATE FUNCTIONS
-  */
+//   /*
+//     PRIVATE FUNCTIONS
+//   */
 
-  #getWalletFromSeedPhrase(seedPhrase) {
-    const seed = mnemonicToSeedSync(seedPhrase)
-    const hdwallet = hdkey.fromMasterSeed(seed)
+//   #getWalletFromSeedPhrase(seedPhrase) {
+//     const seed = mnemonicToSeedSync(seedPhrase)
+//     const hdwallet = hdkey.fromMasterSeed(seed)
 
-    const wallet_hdpath = `m/44'/60'/0'/0/0`
+//     const wallet_hdpath = `m/44'/60'/0'/0/0`
 
-    const wallet = hdwallet.derivePath(wallet_hdpath).getWallet()
-    const address = '0x' + wallet.getAddress().toString('hex')
+//     const wallet = hdwallet.derivePath(wallet_hdpath).getWallet()
+//     const address = '0x' + wallet.getAddress().toString('hex')
 
-    const privateKey = wallet.getPrivateKey().toString('hex')
-    // const restoredWallet = this.#web3.eth.accounts.privateKeyToAccount(privateKey)
-    const restoredWallet = new ethers.Wallet(privateKey, this.#web3)
+//     const privateKey = wallet.getPrivateKey().toString('hex')
+//     // const restoredWallet = this.#web3.eth.accounts.privateKeyToAccount(privateKey)
+//     const restoredWallet = new ethers.Wallet(privateKey, this.#web3)
 
-    return restoredWallet
-  }
+//     return restoredWallet
+//   }
 
-  #generateMnemonic() {
-    return generateMnemonic()
-  }
-}
+//   #generateMnemonic() {
+//     return generateMnemonic()
+//   }
+// }
 
-export default new EthereumTool()
+// export default new EthereumTool()
+
+import { EthereumTool } from '@_koi/sdk/ethereum'
+export { EthereumTool }
--- src/services/k2/index.js
+++ src/services/k2/index.js
@@ -1,113 +1,116 @@
-import {
-  clusterApiUrl,
-  Connection,
-  Keypair,
-  LAMPORTS_PER_SOL,
-  PublicKey,
-  sendAndConfirmTransaction,
-  SystemProgram,
-  Transaction} from '@_koi/web3.js'
-import { generateMnemonic, mnemonicToSeedSync } from 'bip39'
-import bs58 from 'bs58'
-import { derivePath } from 'ed25519-hd-key'
-import { isEmpty } from 'lodash'
+// import {
+//   clusterApiUrl,
+//   Connection,
+//   Keypair,
+//   LAMPORTS_PER_SOL,
+//   PublicKey,
+//   sendAndConfirmTransaction,
+//   SystemProgram,
+//   Transaction} from '@_koi/web3.js'
+// import { generateMnemonic, mnemonicToSeedSync } from 'bip39'
+// import bs58 from 'bs58'
+// import { derivePath } from 'ed25519-hd-key'
+// import { isEmpty } from 'lodash'
 
-export class K2Tool {
-  constructor(credentials, provider) {
-    this.key = null
-    this.address = null
-    this.keypair = null
+// export class K2Tool {
+//   constructor(credentials, provider) {
+//     this.key = null
+//     this.address = null
+//     this.keypair = null
 
-    if (!isEmpty(credentials)) {
-      this.key = credentials.key
-      this.address = credentials.address
-      this.keypair = Keypair.fromSecretKey(new Uint8Array(credentials.key.split(',')))
-    }
+//     if (!isEmpty(credentials)) {
+//       this.key = credentials.key
+//       this.address = credentials.address
+//       this.keypair = Keypair.fromSecretKey(new Uint8Array(credentials.key.split(',')))
+//     }
 
-    this.provider = provider || 'testnet'
-    this.connection = new Connection(clusterApiUrl(provider), 'confirmed')
-  }
+//     this.provider = provider || 'testnet'
+//     this.connection = new Connection(clusterApiUrl(provider), 'confirmed')
+//   }
 
-  getCurrentNetwork() {
-    return this.provider
-  }
+//   getCurrentNetwork() {
+//     return this.provider
+//   }
 
-  /* DatH - LongP support importing multiple wallets by a seedphrase */
-  importSeedphrase(seedphrase) {
-    /* Utilities */
-    const bufferToString = (buffer) => Buffer.from(buffer).toString('hex')
-    const deriveSeed = (seed) => derivePath(DERIVE_PATH, seed).key
+//   /* DatH - LongP support importing multiple wallets by a seedphrase */
+//   importSeedphrase(seedphrase) {
+//     /* Utilities */
+//     const bufferToString = (buffer) => Buffer.from(buffer).toString('hex')
+//     const deriveSeed = (seed) => derivePath(DERIVE_PATH, seed).key
 
-    /* Import 100 wallets from seedphrase and DERIVE_PATH */
-    let keypairs = []
-    for (let i = 0; i < 100; i++) {
-      const DERIVE_PATH = `m/44'/501'/${i}'/0'`
+//     /* Import 100 wallets from seedphrase and DERIVE_PATH */
+//     let keypairs = []
+//     for (let i = 0; i < 100; i++) {
+//       const DERIVE_PATH = `m/44'/501'/${i}'/0'`
 
-      const seed = mnemonicToSeedSync(seedphrase)
-      keypair = Keypair.fromSeed(deriveSeed(DERIVE_PATH, bufferToString(seed)))
+//       const seed = mnemonicToSeedSync(seedphrase)
+//       keypair = Keypair.fromSeed(deriveSeed(DERIVE_PATH, bufferToString(seed)))
 
-      keypairs.push(keypair)
-    }
+//       keypairs.push(keypair)
+//     }
 
-    return keypairs
-  }
+//     return keypairs
+//   }
 
-  importWallet(key, type) {
-    let wallet
-    let keypair
+//   importWallet(key, type) {
+//     let wallet
+//     let keypair
 
-    if (type === 'seedphrase') {
-      const DEFAULT_DERIVE_PATH = `m/44'/501'/0'`
+//     if (type === 'seedphrase') {
+//       const DEFAULT_DERIVE_PATH = `m/44'/501'/0'`
 
-      const bufferToString = (buffer) => Buffer.from(buffer).toString('hex')
-      const deriveSeed = (seed) => derivePath(DEFAULT_DERIVE_PATH, seed).key
+//       const bufferToString = (buffer) => Buffer.from(buffer).toString('hex')
+//       const deriveSeed = (seed) => derivePath(DEFAULT_DERIVE_PATH, seed).key
 
-      const seed = mnemonicToSeedSync(key)
-      keypair = Keypair.fromSeed(deriveSeed(bufferToString(seed)))
-    } else {
-      const secretKey = bs58.decode(key)
-      keypair = Keypair.fromSecretKey(secretKey)
-    }
+//       const seed = mnemonicToSeedSync(key)
+//       keypair = Keypair.fromSeed(deriveSeed(bufferToString(seed)))
+//     } else {
+//       const secretKey = bs58.decode(key)
+//       keypair = Keypair.fromSecretKey(secretKey)
+//     }
 
-    this.keypair = keypair
-    this.address = keypair.publicKey.toString()
-    this.key = keypair.secretKey.toString()
+//     this.keypair = keypair
+//     this.address = keypair.publicKey.toString()
+//     this.key = keypair.secretKey.toString()
 
-    wallet = {
-      address: this.address,
-      privateKey: this.key
-    }
+//     wallet = {
+//       address: this.address,
+//       privateKey: this.key
+//     }
 
-    return wallet
-  }
+//     return wallet
+//   }
 
-  generateWallet() {
-    const seedPhrase = generateMnemonic()
+//   generateWallet() {
+//     const seedPhrase = generateMnemonic()
 
-    this.importWallet(seedPhrase, 'seedphrase')
+//     this.importWallet(seedPhrase, 'seedphrase')
 
-    return seedPhrase
-  }
+//     return seedPhrase
+//   }
 
-  async getBalance() {
-    const balance = await this.connection.getBalance(this.keypair.publicKey)
+//   async getBalance() {
+//     const balance = await this.connection.getBalance(this.keypair.publicKey)
 
-    return balance
-  }
+//     return balance
+//   }
 
-  async transfer(recipient, amount) {
-    const transaction = new Transaction()
+//   async transfer(recipient, amount) {
+//     const transaction = new Transaction()
 
-    transaction.add(
-      SystemProgram.transfer({
-        fromPubkey: this.keypair.publicKey,
-        toPubkey: new PublicKey(recipient),
-        lamports: amount * LAMPORTS_PER_SOL
-      })
-    )
+//     transaction.add(
+//       SystemProgram.transfer({
+//         fromPubkey: this.keypair.publicKey,
+//         toPubkey: new PublicKey(recipient),
+//         lamports: amount * LAMPORTS_PER_SOL
+//       })
+//     )
 
-    const receipt = await sendAndConfirmTransaction(this.connection, transaction, [this.keypair])
+//     const receipt = await sendAndConfirmTransaction(this.connection, transaction, [this.keypair])
 
-    return receipt
-  }
-}
+//     return receipt
+//   }
+// }
+
+import { K2Tool } from '@_koi/sdk/k2'
+export { K2Tool }
