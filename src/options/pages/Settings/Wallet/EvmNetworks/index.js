b74fd3449eb1b68b17175f0c2d1e5cd57508e5df
Arthur <arthur@koii.network>
2023-05-01 19:23:43 +0700

    [f] - evm support - refactor EvmNetwork component

--- src/options/pages/Settings/Wallet/EvmNetworks/index.js
+++ src/options/pages/Settings/Wallet/EvmNetworks/index.js
@@ -1,135 +1,121 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import BnbLogo from 'img/bnb-logo.svg'
+import { PREDEFINED_EVM_NETWORK_METADATA } from 'constants/koiConstants'
+import PolygonLogo from 'img/polygon-logo.svg'
+import EthLogo from 'img/v2/ethereum-logos/ethereum-logo.svg'
+import get from 'lodash/get'
 import { loadAllAccounts } from 'options/actions/accounts'
 import storage from 'services/storage'
 import reloadGalleryPage from 'utils/reloadGalleryPage'
 
-
-const PolygonTestnet = () => {
-  const dispatch = useDispatch()
-  const [added, setAdded] = useState(false)
-
-  useEffect(() => {
-
-    const load = async () => {
-      const addedNetworks = await storage.setting.get.addedEvmNetworks()
-      const isAdded = !addedNetworks.every(network => network.label !== 'Polygon Testnet')
-      setAdded(isAdded)
-    }
-
-    load()
-  }, [])
-
-  const addNetwork = async () => {
-    let addedNetworks = await storage.setting.get.addedEvmNetworks()
-    const polygonNetwork = {
-      label: 'Polygon Testnet',
-      value: 'https://rpc-mumbai.maticvigil.com/'
-    }
-    
-    addedNetworks = [...addedNetworks, polygonNetwork]
-
-    await storage.setting.set.addedEvmNetworks(addedNetworks)
-    setAdded(true)
-
-    await dispatch(loadAllAccounts())
-  }
-
-  const removeNetwork = async () => {
-    let addedNetworks = await storage.setting.get.addedEvmNetworks()
-
-    addedNetworks = addedNetworks.filter(network => {
-      return  network.label !== 'Polygon Testnet'
-    })
-    await storage.setting.set.addedEvmNetworks(addedNetworks)
-
-    await storage.setting.set.ethereumProvider('https://goerli.infura.io/v3/f811f2257c4a4cceba5ab9044a1f03d2')
-    await dispatch(loadAllAccounts())
-    reloadGalleryPage()
-  }
-
-  return (
-    <div className='flex w-full justify-between mb-6'>
-      <div className='flex'>
-        <BnbLogo />
-        <div className='ml-4 text-sm'>Polygon Testnet</div>
-      </div>
-      {!added ? 
-        <div onClick={addNetwork} className='text-sm font-semibold text-turquoiseBlue underline cursor-pointer'>
-          Add Network
-        </div> :
-        <div onClick={removeNetwork} className='text-sm font-semibold text-turquoiseBlue underline cursor-pointer'>
-          Remove Network
-        </div>
+const useNetworkIcon = ({ networkPayload }) => {
+  const icon = useMemo(() => {
+    if (networkPayload) {
+      const networkName = get(networkPayload, 'label')
+      switch (networkName) {
+        case 'Polygon Testnet':
+          return <PolygonLogo />
+        case 'Polygon':
+          return <PolygonLogo />
       }
-    </div>
-  )
+    }
+    return <EthLogo />
+  }, [networkPayload])
+
+  return icon
 }
 
-const PolygonMainnet = () => {
+const EvmNetwork = ({ networkPayload }) => {
   const dispatch = useDispatch()
-  const [added, setAdded] = useState(false)
+
+  const [isAdded, setIsAdded] = useState(false)
+
+  const networkIcon = useNetworkIcon({ networkPayload })
 
   useEffect(() => {
     const load = async () => {
       const addedNetworks = await storage.setting.get.addedEvmNetworks()
-      const isAdded = !addedNetworks.every(network => network.label !== 'Polygon')
-      setAdded(isAdded)
+      const isAdded = !addedNetworks.every(network => network.label !== get(networkPayload, 'label'))
+      setIsAdded(isAdded)
     }
-
     load()
   }, [])
 
   const addNetwork = async () => {
-    let addedNetworks = await storage.setting.get.addedEvmNetworks()
-    const polygonNetwork = {
-      label: 'Polygon',
-      value: 'https://polygon-rpc.com/'
-    }
-    
-    addedNetworks = [...addedNetworks, polygonNetwork]
+    await storage.setting.update.addedEvmNetwork(currentValue => {
+      if (!currentValue) currentValue = []
+      const payload = {
+        label: get(networkPayload, 'label'),
+        value: get(networkPayload, 'value')
+      }
 
-    await storage.setting.set.addedEvmNetworks(addedNetworks)
-    setAdded(true)
-
-    await dispatch(loadAllAccounts())
+      return [...currentValue, payload]
+    })
+    dispatch(loadAllAccounts())
+    setIsAdded(true)
   }
 
   const removeNetwork = async () => {
-    let addedNetworks = await storage.setting.get.addedEvmNetworks()
-
-    addedNetworks = addedNetworks.filter(network => {
-      return  network.label !== 'Polygon'
+    await storage.setting.update.addedEvmNetwork(currentValue => {
+      return currentValue.filter(network => {
+        return get(network, 'label') !== get(networkPayload, 'label')
+      })
     })
-    await storage.setting.set.addedEvmNetworks(addedNetworks)
+
     await storage.setting.set.ethereumProvider('https://goerli.infura.io/v3/f811f2257c4a4cceba5ab9044a1f03d2')
+
     reloadGalleryPage()
   }
+
+  const isEth = useMemo(() => {
+    const networkName = get(networkPayload, 'label')
+    return networkName === 'ETH Mainnet' || networkName === 'Goerli Testnet'
+  }, [])
 
   return (
     <div className='flex w-full justify-between mb-6'>
       <div className='flex'>
-        <BnbLogo />
-        <div className='ml-4 text-sm'>Polygon</div>
+        <div style={{width:'20px',height:'20px'}}>{ networkIcon }</div>
+        <div className='ml-4 text-sm'>{get(networkPayload, 'label')}</div>
       </div>
-      {!added ? 
+      {!isEth && (!isAdded ? 
         <div onClick={addNetwork} className='text-sm font-semibold text-turquoiseBlue underline cursor-pointer'>
           Add Network
         </div> :
         <div onClick={removeNetwork} className='text-sm font-semibold text-turquoiseBlue underline cursor-pointer'>
           Remove Network
-        </div>
+        </div>)
       }
     </div>
   )
 }
 
 const EvmNetworks = () => {
+  const [networks, setNetworks] = useState([])
+
+  useEffect(() => {
+    const loadNetworks = async () => {
+      let predefinedNetworks = Object.values({ ...PREDEFINED_EVM_NETWORK_METADATA })
+      predefinedNetworks = predefinedNetworks.map(network => {
+        return {
+          label: get(network, 'networkName'),
+          value: get(network, 'rpcUrl')
+        }
+      })
+
+      setNetworks(predefinedNetworks)
+    }
+
+    loadNetworks()
+  }, [])
+
   return (
     <div>
-      <PolygonTestnet />
-      <PolygonMainnet />
+      {
+        networks.map((networkPayload, index) => {
+          return <EvmNetwork key={index} networkPayload={networkPayload} />
+        })
+      }
     </div>
   )
 }
