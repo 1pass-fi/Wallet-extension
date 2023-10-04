9bd7f9c0ea87a9390704dd00fda6c405b86c5183
arthur <arthur@koii.network>
2023-10-04 15:25:54 +0700

    temporary disable nft feature

--- src/background/handlers/popupEvents/controller/loadContent.js
+++ src/background/handlers/popupEvents/controller/loadContent.js
@@ -5,26 +5,27 @@ import { backgroundAccount } from 'services/account'
 
 export default async (payload, next) => {
   try {
-    let allAccounts
-    const { address } = payload.data
-    if (address) {
-      const credentials = await backgroundAccount.getCredentialByAddress(address)
-      const account = await backgroundAccount.getAccount(credentials)
-      allAccounts = [account]
-    } else {
-      allAccounts = await backgroundAccount.getAllAccounts()
-    }
+    // let allAccounts
+    // const { address } = payload.data
+    // if (address) {
+    //   const credentials = await backgroundAccount.getCredentialByAddress(address)
+    //   const account = await backgroundAccount.getAccount(credentials)
+    //   allAccounts = [account]
+    // } else {
+    //   allAccounts = await backgroundAccount.getAllAccounts()
+    // }
 
-    const newContentList = []
+    // const newContentList = []
 
-    await Promise.all(allAccounts.map(async account => {
-      const { contents, newContents } = await account.method.loadMyContent()
-      newContentList.push({ newContents, account })
-      await helpers.saveContent.cacheNFTs(contents, account)
-    }))
-    next()
+    // await Promise.all(allAccounts.map(async account => {
+    //   const { contents, newContents } = await account.method.loadMyContent()
+    //   newContentList.push({ newContents, account })
+    //   await helpers.saveContent.cacheNFTs(contents, account)
+    // }))
+    // next()
     
-    newContentList.forEach(({ newContents, account }) => helpers.saveContent.saveNewNFTsToStorage(newContents, account))
+    // newContentList.forEach(({ newContents, account }) => helpers.saveContent.saveNewNFTsToStorage(newContents, account))
+    console.log('load my content')
 
   } catch (err) {
     console.error(err.message)
